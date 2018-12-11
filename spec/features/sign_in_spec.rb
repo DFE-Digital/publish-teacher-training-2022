2d90267633a162bc7487cab3959beba96b511aed
Jake Benilov <benilov@gmail.com>
2018-12-11 17:17:06 +0000

    Extend spec coverage to cover signing out

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -14,10 +14,13 @@ RSpec.feature 'Sign in', type: :feature do
       }
     end
 
-    scenario 'successfully signs in in the user' do
+    scenario 'successfully signs the user in and out' do
       visit root_path
       click_link "Sign in"
       expect(page).to have_content("Welcome, John")
+
+      click_link "Sign out (John Smith)"
+      expect(page).to have_content("Sign in")
     end
   end
 end
