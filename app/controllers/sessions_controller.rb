3a524d0e0d953efd0546cbf1c6345b4a846f857a
Dan Mitchell <dan@danmitchell.co.uk>
2018-12-12 09:55:32 +0000

    Add proper signout journey

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,15 @@
 class SessionsController < ApplicationController
+  def new
+    redirect_to "/auth/dfe"
+  end
+
   def create
     session[:auth_user] = auth_hash
     redirect_to root_path
+  end
+
+  def signout
+    redirect_to "#{ENV['DFE_SIGN_IN_ISSUER']}/session/end?id_token_hint=#{current_user['credentials']['id_token']}&post_logout_redirect_uri=#{ENV['BASE_URL']}/auth/dfe/signout"
   end
 
   def destroy
