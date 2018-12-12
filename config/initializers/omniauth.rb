6f8537911d5880c2017e16a11a667541645f4c7a
Dan Mitchell <dan@danmitchell.co.uk>
2018-12-12 15:52:54 +0000

    Monkey patch for failure stat

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,29 +1,81 @@
+module OmniAuth
+  module Strategies
+    class OpenIDConnect
+      def callback_phase
+        error = request.params['error_reason'] || request.params['error']
+        if error
+          raise CallbackError.new(request.params['error'], request.params['error_description'] || request.params['error_reason'], request.params['error_uri'])
+        elsif request.params['state'].to_s.empty? || request.params['state'] != stored_state
+          # Monkey patch: Ensure a basic 401 rack response with no body or header isn't served
+          # return Rack::Response.new(['401 Unauthorized'], 401).finish
+          return redirect('/auth/failure')
+        elsif !request.params['code']
+          return fail!(:missing_code, OmniAuth::OpenIDConnect::MissingCodeError.new(request.params['error']))
+        else
+          options.issuer = issuer if options.issuer.blank?
+          discover! if options.discovery
+          client.redirect_uri = redirect_uri
+          client.authorization_code = authorization_code
+          access_token
+          super
+        end
+      rescue CallbackError => e
+        fail!(:invalid_credentials, e)
+      rescue ::Timeout::Error, ::Errno::ETIMEDOUT => e
+        fail!(:timeout, e)
+      rescue ::SocketError => e
+        fail!(:failed_to_connect, e)
+      end
+    end
+  end
+end
+
 class OmniAuth::Strategies::Dfe < OmniAuth::Strategies::OpenIDConnect; end
 
-Rails.application.config.middleware.use OmniAuth::Builder do
-  dfe_sign_in_issuer_uri = URI(ENV["DFE_SIGN_IN_ISSUER"])
-  dfe_sign_in_identifier = ENV["DFE_SIGN_IN_IDENTIFIER"]
-  dfe_sign_in_secret     = ENV["DFE_SIGN_IN_SECRET"]
-  base_url               = ENV["BASE_URL"]
+if ENV['DFE_SIGN_IN_ISSUER'].present?
+  dfe_sign_in_issuer_uri = URI.parse(ENV['DFE_SIGN_IN_ISSUER'])
+  options = {
+    name: :dfe,
+    discovery: true,
+    response_type: :code,
+    client_signing_alg: :RS256,
+    scope: %i[openid profile email],
+    client_options: {
+      port: dfe_sign_in_issuer_uri.port,
+      scheme: dfe_sign_in_issuer_uri.scheme,
+      host: dfe_sign_in_issuer_uri.host,
+      identifier: ENV['DFE_SIGN_IN_IDENTIFIER'],
+      secret: ENV['DFE_SIGN_IN_SECRET'],
+      redirect_uri: "#{ENV['BASE_URL']}/auth/dfe/callback",
+      authorization_endpoint: '/auth',
+      jwks_uri: '/certs',
+      userinfo_endpoint: '/me'
+    }
+  }
 
-  dfe_sign_in_issuer_url = "#{dfe_sign_in_issuer_uri}:#{dfe_sign_in_issuer_uri.port}" if dfe_sign_in_issuer_uri.port
+  Rails.application.config.middleware.use OmniAuth::Strategies::OpenIDConnect, options
 
-  provider :dfe,
-           name: :dfe,
-           discovery: true,
-           response_type: :code,
-           issuer: dfe_sign_in_issuer_url,
-           client_signing_alg: :RS256,
-           scope: %i[openid profile email],
-           client_options: {
-             port: dfe_sign_in_issuer_uri.port,
-             scheme: dfe_sign_in_issuer_uri.scheme,
-             host: dfe_sign_in_issuer_uri.host,
-             identifier: dfe_sign_in_identifier,
-             secret: dfe_sign_in_secret,
-             redirect_uri: "#{base_url}/auth/dfe/callback",
-             authorization_endpoint: "/auth",
-             jwks_uri: "/certs",
-             userinfo_endpoint: "/me"
-           }
+  class DfeSignIn
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      request = Rack::Request.new(env)
+
+      if request.path == '/auth/failure'
+        response = Rack::Response.new
+        response.redirect('/401')
+        response.finish
+      elsif request.path == '/auth/dfe/callback' && request.params.empty? && !OmniAuth.config.test_mode
+        response = Rack::Response.new
+        response.redirect('/dfe/sessions/new')
+        response.finish
+      else
+        @app.call(env)
+      end
+    end
+  end
+
+  Rails.application.config.middleware.insert_before OmniAuth::Strategies::OpenIDConnect, DfeSignIn
 end
