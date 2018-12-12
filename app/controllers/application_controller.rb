0a257caa6f94b8e66eb4bb063a7324167a6f6baf
Dan Mitchell <dan@danmitchell.co.uk>
2018-12-12 17:33:31 +0000

    Add custom error pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception, except: :not_found
+
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  def not_found
+    respond_to do |format|
+      format.html { render 'errors/not_found', status: :not_found }
+      format.json { render json: { error: 'Resource not found' }, status: :not_found }
+      format.all { render status: :not_found, body: nil }
+    end
+  end
 end
---
+++ app/controllers/errors_controller.rb
@@ -0,0 +1,26 @@
+class ErrorsController < ApplicationController
+  skip_before_action :verify_authenticity_token,
+                     only: %i[not_found internal_server_error forbidden unauthorised]
+
+  def not_found
+    respond_to do |format|
+      format.html { render status: :not_found }
+      format.json { render json: { error: 'Resource not found' }, status: :not_found }
+      format.all { render status: :not_found, body: nil }
+    end
+  end
+
+  def internal_server_error
+    respond_to do |format|
+      format.html { render status: :internal_server_error }
+      format.json { render json: { error: 'Internal server error' }, status: :internal_server_error }
+    end
+  end
+
+  def forbidden
+    respond_to do |format|
+      format.html { render status: :forbidden }
+      format.json { render json: { error: 'Forbidden' }, status: :forbidden }
+    end
+  end
+end
