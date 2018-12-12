8849d47ce88a1e98f220d9321f000ed5047beb63
Dan Mitchell <dan@danmitchell.co.uk>
2018-12-12 17:33:31 +0000

    Add custom error pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   get "/cookies", to: "pages#cookies", as: :cookies
   get "/terms-conditions", to: "pages#terms", as: :terms
   get "/privacy-policy", to: "pages#privacy", as: :privacy
+
+  match '/404', to: 'errors#not_found', via: :all
+  match '/403', to: 'errors#forbidden', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
+  match '*path', to: 'errors#not_found', via: :all
 end
