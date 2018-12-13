c780d7b1ac21e2737c62ba7f0df300e9dc738bbb
Dan Mitchell <dan@danmitchell.co.uk>
2018-12-13 08:44:08 +0000

    Add page title, footers links and remove copyright

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get "/pages/:page", to: "pages#show"
+
+  get "/cookies", to: "pages#cookies", as: :cookies
+  get "/terms-conditions", to: "pages#terms", as: :terms
+  get "/privacy-policy", to: "pages#privacy", as: :privacy
 end
