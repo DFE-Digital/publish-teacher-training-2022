b4356c1043f870df6df96ed7c17030ff1523c516
Dan Mitchell <dan@danmitchell.co.uk>
2018-12-14 11:25:51 +0000

    Remove activerecord

--- README.md
+++ README.md
@@ -5,7 +5,6 @@
 ## Prerequisites
 
 - Ruby 2.3.3
-- PostgreSQL
 - NodeJS 8.11.x
 - Yarn 1.12.x
 
@@ -13,8 +12,7 @@
 
 1. Run `bundle install` to install the gem dependencies
 2. Run `yarn` to install node dependencies
-3. Run `bin/rails db:setup` to set up the database development and test schemas, and seed with test data.
-4. Run `bundle exec foreman start -f Procfile.dev` to launch the app on http://localhost:5000.
+3. Run `bundle exec foreman start -f Procfile.dev` to launch the app on http://localhost:5000.
 
 ## Linting
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,8 +2,17 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
-# Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+
+# Remove the ActiveRecord constant, because it is autloaded by
+# ActiveStorage and not needed for our application. The presence
+# of the ActiveRecord constant causes rspec-rails to include
+# extra fixture support, which results in:
+#
+#   ActiveRecord::ConnectionNotEstablished:
+#     No connection pool with 'primary' found.
+#
+Object.send(:remove_const, :ActiveRecord)
+
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -22,23 +31,7 @@ require 'rspec/rails'
 #
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
-# Checks for pending migrations and applies them before tests are run.
-# If you are not using ActiveRecord, you can remove these lines.
-begin
-  ActiveRecord::Migration.maintain_test_schema!
-rescue ActiveRecord::PendingMigrationError => e
-  puts e.to_s.strip
-  exit 1
-end
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
-
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
