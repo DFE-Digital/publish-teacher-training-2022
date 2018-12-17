2d27730aa5c936ab12f336ba6ceb7176cfb67216
Dan Mitchell <dan@danmitchell.co.uk>
2018-12-17 19:59:15 +0000

    Update tests

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -4,9 +4,7 @@ RSpec.feature 'Sign in', type: :feature do
   scenario 'using DfE Sign-in' do
     stub_omniauth
     visit root_path
-    expect(page).to have_link("Sign in")
-    click_link "Sign in"
-    expect(page).to have_content("Welcome, John")
+    # Redirect to DfE Signin and come back
     expect(page).to have_content("Sign out (John Smith)")
   end
 end
