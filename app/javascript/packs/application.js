23b25c737437805c2df2584799e7ce3f732558f9
Dan Mitchell <dan@danmitchell.co.uk>
2019-01-03 08:15:35 +0000

    Add cookie banner

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,7 @@
 import { initAll } from "govuk-frontend";
+import CookieMessage from "scripts/cookie-banner";
 
 initAll();
+
+var $cookieMessage = document.querySelector('[data-module="cookie-message"]');
+new CookieMessage($cookieMessage).init();
