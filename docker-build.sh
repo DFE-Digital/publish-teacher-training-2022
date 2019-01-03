7aadd4f10259f6bab927a89afc60624b2e27e6b5
Tim Abell <tim@timwise.co.uk>
2019-01-03 15:43:50 +0000

    DRY the docker commands

---
+++ docker-build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -e
+
+if [ -z "$TRAVIS_BRANCH" ];
+then
+  echo "TRAVIS_BRANCH environment variable is required"
+  exit 1
+fi
+
+source config.sh
+
+DOCKER_REGISTRY_HOST=$DOCKER_REGISTRY_HOST ./docker-login.sh
+
+DOCKER_MASTER_TAG="$DOCKER_PATH:master"
+DOCKER_BRANCH_TAG="$DOCKER_PATH:$TRAVIS_BRANCH"
+
+echo "Pulling $DOCKER_MASTER_TAG to populate layer cache..."
+# Pull the latest build so that we can re-use the layers as part of the cache when building.
+# Based on http://atodorov.org/blog/2017/08/07/faster-travis-ci-tests-with-docker-cache/
+docker pull "$DOCKER_MASTER_TAG"
+
+echo "Running docker build..."
+docker build --cache-from "$DOCKER_MASTER_TAG" -t "$DOCKER_BRANCH_TAG" -f Dockerfile .
+
+function docker-run() {
+  echo "Running docker command: $1 ..."
+  docker run "$DOCKER_BRANCH_TAG" /bin/sh -c "$1"
+}
+
+function bundle-run() {
+  docker-run "$1"
+}
+
+echo "Running webpack..."
+bundle-run "rails webpacker:compile"
+
+echo "Running tests..."
+bundle-run "rails spec"
+
+echo "Running linters..."
+bundle-run "govuk-lint-ruby app config db lib spec --format clang"
+bundle-run "govuk-lint-sass app/assets/stylesheets"
--- docker-push.sh
+++ docker-push.sh
@@ -1,3 +1,15 @@
-#!/bin/sh
-echo "$AZURE_CR_PASSWORD" | docker login batdevcontainerregistry.azurecr.io -u="batdevcontainerregistry" --password-stdin
-docker push batdevcontainerregistry.azurecr.io/manage-courses-frontend:$TRAVIS_BRANCH
+#!/bin/bash
+set -e
+
+if [ -z "$TRAVIS_BRANCH" ];
+then
+  echo "TRAVIS_BRANCH environment variable is required"
+  exit 1
+fi
+
+source config.sh
+
+DOCKER_REGISTRY_HOST=$DOCKER_REGISTRY_HOST ./docker-login.sh
+
+echo "Running docker push to $DOCKER_PATH:$TRAVIS_BRANCH ..."
+docker push "$DOCKER_PATH:$TRAVIS_BRANCH"
