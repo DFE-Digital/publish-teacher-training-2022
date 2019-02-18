525d51dc49e0597a1b32ffbd023a75d36ed2d76e
Theodor Vararu <theo@vararu.org>
2019-02-18 17:52:11 +0000

    [970] Update webpack-dev-server to 3.0.0 and enable https
    webpack-dev-server 3.0.0 adds support for https, which is necessary
    in order to run the app on the local machine. The configuration for
    https is done via the JS interface in order to pass in the content of
    the certs.

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,12 @@
+const fs = require('fs')
+
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
+
+environment.config.devServer.https = {
+  key: fs.readFileSync('./config/localhost/https/localhost.key'),
+  cert: fs.readFileSync('./config/localhost/https/localhost.crt')
+}
 
 module.exports = environment.toWebpackConfig()
