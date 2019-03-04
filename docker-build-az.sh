71f09d9c8a83def1cd8c6c1f87179c1106b9311b
Craig Gumbley <craiggumbley@gmail.com>
2019-03-04 22:49:47 +0000

    Minor formatting improvements and evaluate PIPESTATUS for tests

--- docker-build-az.sh
+++ docker-build-az.sh
@@ -2,7 +2,7 @@
 # Build, test and push a custom docker image
 set -e
 
-if [ $SYSTEM_DEBUG="true" ];
+if [ "$SYSTEM_DEBUG" = 'true' ];
 then
   env
 fi
@@ -13,18 +13,18 @@ DOCKER_HUB_PASSWORD=$password
 DOCKER_RUN="docker run $DOCKER_IMAGE_TAG /bin/sh -c"
 
 echo "Building image: $DOCKER_IMAGE_TAG"
-docker build -f Dockerfile -t $DOCKER_IMAGE_TAG .
+docker build -f Dockerfile -t "$DOCKER_IMAGE_TAG" .
 
 echo "Run webpack"
 $DOCKER_RUN "rails webpacker:compile"
 
 echo "Run tests"
-$DOCKER_RUN "rails spec SPEC_OPTS='--format RspecJunitFormatter'" | sed -e 1d >> rspec-results.xml
+$DOCKER_RUN "rails spec SPEC_OPTS='--format RspecJunitFormatter'" | sed -e 1d >> rspec-results.xml ; test "${PIPESTATUS[0]}" -eq 0
 
 echo "Run linters"
 $DOCKER_RUN "govuk-lint-ruby app config db lib spec --format clang"
 $DOCKER_RUN "govuk-lint-sass app/webpacker/stylesheets"
 
 echo "Push image"
-echo $DOCKER_HUB_PASSWORD | docker login --username $DOCKER_HUB_USERNAME --password-stdin
-docker push $DOCKER_IMAGE_TAG
+echo "$DOCKER_HUB_PASSWORD" | docker login --username "$DOCKER_HUB_USERNAME" --password-stdin
+docker push "$DOCKER_IMAGE_TAG"
