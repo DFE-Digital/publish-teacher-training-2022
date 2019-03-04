0ad97dcea4e19999049217d02d50392b95e808d7
defong <de_fong2k@yahoo.com>
2019-03-04 15:20:02 +0000

    [1020] Added the default for seetings in reagrds to authenication with the backend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,8 +30,21 @@ class ApplicationController < ActionController::Base
 private
 
   def set_connection
+    if Settings.authentication.algorithm == 'plain-text'
+      # This method can be used in development mode to simplify querying
+      # the API with curl. It should allow us to do:
+      #
+      #    curl -H 'Authorization: Bearer user@education.gov.uk' http://localhost:3001/api/v2/providers
+      token = current_user_info['email'].to_s
+    else
+      payload = { email: current_user_info['email'].to_s }
+      token = JWT.encode(payload.to_json,
+                          Settings.authentication.secret,
+                          Settings.authentication.algorithm)
+    end
+
     Base.connection do |connection|
-      connection.use :oauth2, current_user_info['email'].to_s, token_type: :bearer
+      connection.use FaradayMiddleware::OAuth2, token, token_type: :bearer
     end
   end
 end
