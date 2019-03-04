1d433a1e93ae39d83f4c0b945f9fbcc163240ef2
defong <de_fong2k@yahoo.com>
2019-03-04 16:24:17 +0000

    [1020] Fixed linter issues

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe ApplicationController, type: :controller do
 
 
   describe '#authenticate' do
-  subject { controller.authenticate }
+    subject { controller.authenticate }
 
     context 'user is unauthenticated' do
       it { should redirect_to '/signin' }
@@ -21,18 +21,18 @@ RSpec.describe ApplicationController, type: :controller do
         payload = { email: user_email.to_s }
 
         allow(JWT).to receive(:encode)
-        .with(payload.to_json, Settings.authentication.secret,Settings.authentication.algorithm)
+        .with(payload.to_json, Settings.authentication.secret, Settings.authentication.algorithm)
         .and_return("anything")
 
-        controller.request.session = {:auth_user => {"info" => {
+        controller.request.session = { auth_user: { "info" => {
           "first_name" => "John",
           "last_name" => "Smith",
           "email" => user_email
-        }}}
+        } } }
       end
 
       it "sets encoded payload for connection" do
-        act =  controller.authenticate
+        controller.authenticate
         expect(JWT).to have_received(:encode)
         expect(Base).to have_received(:connection)
       end
