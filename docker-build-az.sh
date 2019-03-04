072c420b202a43fa1a5f0b47d443073a5821e5ee
Craig Gumbley <craiggumbley@gmail.com>
2019-03-04 22:27:19 +0000

    Tidy up script

--- docker-build-az.sh
+++ docker-build-az.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
+# Build, test and push a custom docker image
 set -e
 
-# DEBUG: print env
+if [ $SYSTEM_DEBUG="true" ];
+then
+  env
+fi
 
-env
-
-# Set up script vars
 DOCKER_IMAGE_TAG=$TAG
-DOCKER_RUN="docker run $DOCKER_IMAGE_TAG /bin/sh -c"
 DOCKER_HUB_USERNAME=$DOCKERHUBUSERNAME
 DOCKER_HUB_PASSWORD=$password
+DOCKER_RUN="docker run $DOCKER_IMAGE_TAG /bin/sh -c"
 
 echo "Building image: $DOCKER_IMAGE_TAG"
 docker build -f Dockerfile -t $DOCKER_IMAGE_TAG .
 
-echo "Run webpacker"
+echo "Run webpack"
 $DOCKER_RUN "rails webpacker:compile"
 
 echo "Run tests"
-$DOCKER_RUN 'rails spec SPEC_OPTS="--format RspecJunitFormatter"' | sed -e 1d >> rspec-results.xml
+$DOCKER_RUN "rails spec SPEC_OPTS='--format RspecJunitFormatter'" | sed -e 1d >> rspec-results.xml
 
 echo "Run linters"
 $DOCKER_RUN "govuk-lint-ruby app config db lib spec --format clang"
 $DOCKER_RUN "govuk-lint-sass app/webpacker/stylesheets"
 
-echo "Pushing image"
+echo "Push image"
 echo $DOCKER_HUB_PASSWORD | docker login --username $DOCKER_HUB_USERNAME --password-stdin
 docker push $DOCKER_IMAGE_TAG
