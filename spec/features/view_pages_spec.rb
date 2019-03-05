88c79b838cfa8c2e1120af993a533fae0224a9ba
Theodor Vararu <theo@vararu.org>
2019-03-05 15:58:40 +0000

    [960] Stub request to backend API in feature specs

--- spec/features/view_pages_spec.rb
+++ spec/features/view_pages_spec.rb
@@ -4,6 +4,7 @@ RSpec.feature 'View pages', type: :feature do
   scenario "Navigate to home" do
     stub_omniauth
     stub_session_create
+    stub_backend_api
 
     visit "/pages/home"
     # Redirect to DfE Signin and come back
--- spec/support/feature_helpers.rb
+++ spec/support/feature_helpers.rb
@@ -18,6 +18,38 @@ module FeatureHelpers
   def stub_session_create
     allow(Session).to receive(:create).and_return(double(id: 1))
   end
+
+  def stub_backend_api
+    example_api_response = '{
+      "data": [
+        {
+          "id": "4",
+          "type": "providers",
+          "attributes": {
+            "institution_code": "A0",
+            "institution_name": "ACME SCITT 0"
+          },
+          "relationships": {
+            "courses": {
+              "meta": {
+                "count": 0
+              }
+            }
+          }
+        }
+      ],
+      "jsonapi": {
+        "version": "1.0"
+      }
+    }'
+
+    stub_request(:get, "#{Settings.manage_backend.base_url}/api/v2/providers")
+      .to_return(
+        status: 200,
+        body: example_api_response,
+        headers: { 'Content-Type': 'application/vnd.api+json' }
+      )
+  end
 end
 
 RSpec.configure do |config|
