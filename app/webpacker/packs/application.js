cb9098cfb3cd91693bcb6f6fea7b7d5b24c23831
dankmitchell <dan@danmitchell.co.uk>
2019-03-08 13:55:23 +0000

    [1115] Add turbolinks

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -1,8 +1,13 @@
-import "../stylesheets/application.scss";
-import { initAll } from "govuk-frontend";
-import CookieMessage from "scripts/cookie-banner";
+import '../stylesheets/application.scss';
+import Turbolinks from 'turbolinks';
+import { initAll } from 'govuk-frontend';
+import CookieMessage from 'scripts/cookie-banner';
 
-initAll();
+Turbolinks.start();
 
-var $cookieMessage = document.querySelector('[data-module="cookie-message"]');
-new CookieMessage($cookieMessage).init();
+document.addEventListener('turbolinks:load', function () {
+  initAll();
+
+  var $cookieMessage = document.querySelector('[data-module="cookie-message"]');
+  new CookieMessage($cookieMessage).init();
+});
