3ee7f5f3d17987523c81f45717464a40cd1ce46e
De Fong <de_fong2k@yahoo.com>
2019-03-11 16:04:59 +0000

    [1124] Fixed issues whereby the user has circumvented session create hence the user is half authenicated (#91)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, except: :not_found
+  rescue_from JsonApiClient::Errors::NotAuthorized, with: :render_not_authorised
 
   def not_found
     respond_to do |format|
@@ -7,6 +8,10 @@ class ApplicationController < ActionController::Base
       format.json { render json: { error: 'Resource not found' }, status: :not_found }
       format.all { render status: :not_found, body: nil }
     end
+  end
+
+  def render_not_authorised
+    redirect_to Settings.manage_ui.base_url
   end
 
   helper_method :current_user
@@ -18,6 +23,8 @@ class ApplicationController < ActionController::Base
   def authenticate
     if current_user
       add_token_to_connection
+
+      set_user_session if current_user[:user_id].blank?
     else
       redirect_to '/signin'
     end
@@ -28,6 +35,11 @@ class ApplicationController < ActionController::Base
   end
 
 private
+
+  def set_user_session
+    user = Session.create(first_name: current_user_info[:first_name], last_name: current_user_info[:last_name])
+    session[:auth_user][:user_id] = user.id
+  end
 
   def add_token_to_connection
     if Settings.authentication.algorithm == 'plain-text'
--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe ApplicationController, type: :controller do
     controller.response = response
   end
 
-
   describe '#authenticate' do
     subject { controller.authenticate }
 
@@ -14,27 +13,66 @@ RSpec.describe ApplicationController, type: :controller do
     end
 
     context 'user is authenticated' do
-      user_email = "email@example.com"
+      let(:user_email) { "email@example.com" }
+
+      let(:user_info) do
+        {
+          "first_name" => "John",
+          "last_name" => "Smith",
+          "email" => user_email,
+        }
+      end
+
+      let(:payload) { { email: user_email.to_s } }
+
+      let(:user_id) { nil }
+
       before do
         allow(Base).to receive(:connection)
 
-        payload = { email: user_email.to_s }
-
         allow(JWT).to receive(:encode)
-        .with(payload, Settings.authentication.secret, Settings.authentication.algorithm)
-        .and_return("anything")
+          .with(payload, Settings.authentication.secret, Settings.authentication.algorithm)
+          .and_return("anything")
 
-        controller.request.session = { auth_user: { "info" => {
-          "first_name" => "John",
-          "last_name" => "Smith",
-          "email" => user_email
-        } } }
+        controller.request.session = { auth_user: { "info" => user_info } }
       end
 
-      it "sets encoded payload for connection" do
-        controller.authenticate
-        expect(JWT).to have_received(:encode)
-        expect(Base).to have_received(:connection)
+      context 'user_id is not blank' do
+        let(:user_id) { 666 }
+
+        before do
+          allow(Session).to receive(:create)
+
+          controller.request.session = { auth_user: { "info" => user_info, :user_id => user_id } }
+          controller.authenticate
+        end
+
+        it "has performed jwt encoding" do expect(JWT).to have_received(:encode) end
+        it "has set connection" do expect(Base).to have_received(:connection) end
+        it "has not called session create" do expect(Session).to_not have_received(:create) end
+        it "has set user_id" do expect(controller.request.session[:auth_user][:user_id]).to eq user_id end
+      end
+
+      context 'user_id is blank' do
+        let(:user_info) {
+          {
+            "first_name" => "John",
+            "last_name" => "Smith",
+            "email" => user_email,
+          }
+        }
+
+        before do
+          allow(Session).to receive(:create)
+            .with(first_name: user_info[:first_name], last_name: user_info[:last_name])
+            .and_return(double(id: 999))
+          controller.authenticate
+        end
+
+        it "has performed jwt encoding" do expect(JWT).to have_received(:encode) end
+        it "has set connection" do expect(Base).to have_received(:connection) end
+        it "has called session create" do expect(Session).to have_received(:create) end
+        it "has set user_id" do expect(controller.request.session[:auth_user][:user_id]).to eq 999 end
       end
     end
   end
