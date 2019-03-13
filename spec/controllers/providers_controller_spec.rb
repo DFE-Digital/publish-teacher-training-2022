e6a7ead6ec896c49201a314cfd750e61c4a59a19
Theodor Vararu <theo@vararu.org>
2019-03-13 16:25:48 +0000

    [1146] Redirect to organisations#show page when there is 1 provider
    The breadcrumbs are also hidden because they are not useful in this
    scenario. This is done by querying the full Providers endpoint for the
    full list, which is a bit annoying/slow, but can be changed in the
    future if necessary.

--- spec/controllers/providers_controller_spec.rb
+++ spec/controllers/providers_controller_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe ProvidersController, type: :controller do
     end
 
     describe 'GET #index' do
-      context 'with providers' do
+      context 'with 2 or more providers' do
         before do
           stub_api_v2_request('/providers', build(:providers_response))
         end
@@ -19,7 +19,20 @@ RSpec.describe ProvidersController, type: :controller do
         end
       end
 
-      context 'without any providers' do
+      context 'with 1 provider' do
+        let(:the_provider) { build(:provider) }
+
+        before do
+          stub_api_v2_request('/providers', build(:providers_response, data: [the_provider]))
+        end
+
+        it 'returns the show page' do
+          get :index
+          expect(response).to redirect_to(action: :show, code: the_provider["attributes"][:institution_code])
+        end
+      end
+
+      context 'with 0 providers' do
         before do
           stub_api_v2_request('/providers', build(:providers_response, data: []))
         end
