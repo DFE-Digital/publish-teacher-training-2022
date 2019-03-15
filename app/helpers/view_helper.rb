0bf63a8515430a72a731664c64e615751f5e75d7
dankmitchell <dan@danmitchell.co.uk>
2019-03-15 21:30:04 +0000

    1136 add vacancies form with sites

--- app/helpers/view_helper.rb
+++ app/helpers/view_helper.rb
@@ -6,4 +6,23 @@ module ViewHelper
   def manage_ui_link_to_back(url)
     manage_ui_link_to('Back', url, class: "govuk-back-link")
   end
+
+  def are_vacancies_available_for_course_site_status?(course, site_status)
+    case course.study_mode
+    when 'full_time'
+      site_status.full_time_vacancies?
+    when 'part_time'
+      site_status.part_time_vacancies?
+    when 'full_time_or_part_time'
+      site_status.full_time_vacancies? && site_status.part_time_vacancies?
+    end
+  end
+
+  def site_name_with_vac_status(site_status)
+    if site_status.vac_status.present?
+      site_status.site.location_name + ' (' + site_status.vac_status.humanize.gsub(' vacancies', '') + ')'
+    else
+      site_status.site.location_name
+    end
+  end
 end
