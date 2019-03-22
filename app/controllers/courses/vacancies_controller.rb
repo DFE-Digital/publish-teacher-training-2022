38219bd9e7bfa718dd9fdca0f35d05ea30f76690
dankmitchell <dan@danmitchell.co.uk>
2019-03-22 16:17:56 +0000

    [1136] update vac status checkboxes to match course study mode

--- app/controllers/courses/vacancies_controller.rb
+++ app/controllers/courses/vacancies_controller.rb
@@ -2,8 +2,19 @@ module Courses
   class VacanciesController < ApplicationController
     before_action :authenticate, :get_course
 
-    def edit
-      @site_statuses = @course.site_statuses
+    def edit; end
+
+    def update
+      course = Course.includes(:site_statuses).where(provider_code: params[:provider_code]).find(params[:code])
+      site_statuses = course.map(&:site_statuses).flatten
+
+      params[:course][:site_status_attributes].values.each do |vac_status|
+        site_status = site_statuses.find{|site_status| site_status.id == vac_status[:id]}
+        vacancy_status = "no_vacancies" #TODO figure this out
+        site_status.vac_status = vacancy_status
+        site_status.save
+      end
+      redirect_to vacancies_provider_course_path(@course.course_code)
     end
 
   private
--- app/models/site_status.rb
+++ app/models/site_status.rb
@@ -1,7 +1,4 @@
 class SiteStatus < Base
-  belongs_to :site
-  belongs_to :course
-
   def full_time_vacancies?
     vac_status == 'full_time_vacancies'
   end
