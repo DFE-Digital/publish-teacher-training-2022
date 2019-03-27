d9c99a50c9ad810b3cead337bf6ab2e7ba461f59
dankmitchell <dan@danmitchell.co.uk>
2019-03-27 11:36:49 +0000

    Add back link helper for manage ui

--- app/helpers/view_helper.rb
+++ app/helpers/view_helper.rb
@@ -2,4 +2,8 @@ module ViewHelper
   def manage_ui_link_to(body, url, html_options = { class: 'govuk-link' })
     link_to body, "#{Settings.manage_ui.base_url}#{url}", html_options
   end
+
+  def manage_ui_link_to_back(url)
+    manage_ui_link_to('Back', url, class: "govuk-back-link")
+  end
 end
