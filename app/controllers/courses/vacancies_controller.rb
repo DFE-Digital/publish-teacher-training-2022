de18b3d72f49baccf23e344642c23843c6a11446
dankmitchell <dan@danmitchell.co.uk>
2019-03-28 11:59:10 +0000

    Fix courses vacancies edit
    The attributes on the checkboxes changed in the big PR and this was missed

--- app/controllers/courses/vacancies_controller.rb
+++ app/controllers/courses/vacancies_controller.rb
@@ -14,8 +14,8 @@ module Courses
         &.values&.each do |vacancy_status|
           site_status            = find_site_status vacancy_status[:id]
           site_status.vac_status = VacancyStatusDeterminationService.call(
-            vacancy_status_full_time: vacancy_status[:vac_status_full_time],
-            vacancy_status_part_time: vacancy_status[:vac_status_part_time],
+            vacancy_status_full_time: vacancy_status[:full_time],
+            vacancy_status_part_time: vacancy_status[:part_time],
             course:                   @course
           )
           site_status.save
