193199b5641d855ca869bad288d2d104697a074c
Jake Benilov <benilov@gmail.com>
2019-03-29 14:17:20 +0000

    [1207] Restructure link helpers to make them easier to reuse

--- app/helpers/view_helper.rb
+++ app/helpers/view_helper.rb
@@ -1,9 +1,13 @@
 module ViewHelper
-  def manage_ui_link_to(body, url, html_options = { class: 'govuk-link' })
-    link_to body, "#{Settings.manage_ui.base_url}#{url}", html_options
+  def govuk_link_to(body, url, html_options = { class: 'govuk-link' })
+    link_to body, url, html_options
   end
 
-  def manage_ui_link_to_back(url)
-    manage_ui_link_to('Back', url, class: "govuk-back-link")
+  def govuk_back_link_to(url)
+    govuk_link_to('Back', url, class: "govuk-back-link")
+  end
+
+  def manage_ui_url(relative_path)
+    Settings.manage_ui.base_url + relative_path
   end
 end
