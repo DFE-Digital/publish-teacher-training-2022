14b00ec9ab3f44cefb07a2c717ec9c565c6babf9
Heidar Bernhardsson <heidarbn@gmail.com>
2019-04-03 18:06:43 +0100

    [1219] Verify that sync course happens

--- spec/features/courses/vacancies/edit_spec.rb
+++ spec/features/courses/vacancies/edit_spec.rb
@@ -27,6 +27,13 @@ feature 'Edit course vacancies', type: :feature do
   let(:edit_vacancies_path) do
     "/organisations/AO/courses/#{course_attributes[:course_code]}/vacancies"
   end
+  let!(:sync_courses_request_stub) do
+    stub_request(
+      :post,
+      "http://localhost:3001/api/v2/providers/AO/courses/" \
+        "#{course_attributes[:course_code]}/sync_with_search_and_compare"
+    ).to_return(status: 201, body: "")
+  end
 
   before do
     stub_omniauth
@@ -35,12 +42,6 @@ feature 'Edit course vacancies', type: :feature do
       "/providers/AO/courses/#{course_attributes[:course_code]}",
       course
     )
-
-    stub_request(
-      :post,
-      "http://localhost:3001/api/v2/providers/AO/courses/" \
-        "#{course_attributes[:course_code]}/sync_with_search_and_compare"
-    ).to_return(status: 201, body: "")
 
     visit edit_vacancies_path
   end
@@ -96,6 +97,7 @@ feature 'Edit course vacancies', type: :feature do
         "#{site.attributes[:location_name]} (Full time)",
         checked: false
       )
+      expect(sync_courses_request_stub).to have_been_requested
     end
 
     scenario 'removing all vacancies' do
@@ -120,6 +122,7 @@ feature 'Edit course vacancies', type: :feature do
         "#{site.attributes[:location_name]} (Part time)",
         checked: false
       )
+      expect(sync_courses_request_stub).to have_been_requested
     end
   end
 
@@ -159,6 +162,7 @@ feature 'Edit course vacancies', type: :feature do
         "#{site.attributes[:location_name]} (Part time)",
         checked: true
       )
+      expect(sync_courses_request_stub).to have_been_requested
     end
   end
 end
