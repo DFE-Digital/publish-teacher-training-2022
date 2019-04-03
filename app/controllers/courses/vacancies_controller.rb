7d5950ffee8a978a905fe202e725ff0e9e0f5b28
dankmitchell <dan@danmitchell.co.uk>
2019-04-03 20:09:43 +0100

    [1238] only show running site statuses

--- app/controllers/courses/vacancies_controller.rb
+++ app/controllers/courses/vacancies_controller.rb
@@ -36,7 +36,7 @@ module Courses
     end
 
     def build_site_statuses
-      @site_statuses = @course.site_statuses
+      @site_statuses = @course.site_statuses.select { |site_status| site_status.status == 'running' }
     end
 
     def find_site_status(id)
