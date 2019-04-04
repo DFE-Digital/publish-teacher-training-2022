2ca2e12f4028beca19007646548473cc39966811
dankmitchell <dan@danmitchell.co.uk>
2019-04-04 10:14:59 +0100

    [1244] Setup course show page

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -5,4 +5,8 @@ class CoursesController < ApplicationController
     @provider = Provider.find(params[:code]).first.attributes
     @courses = Course.where(provider_code: params[:provider_code])
   end
+
+  def show
+    @course = Course.where(provider_code: params[:provider_code]).find(params[:code]).first
+  end
 end
