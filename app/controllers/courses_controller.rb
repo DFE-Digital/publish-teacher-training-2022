776b24fbcc97f46c5ed60b71776b71dc3532540d
dankmitchell <dan@danmitchell.co.uk>
2019-04-05 08:25:42 +0100

    [1252] add course delete and withdraw pages

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,11 +1,20 @@
 class CoursesController < ApplicationController
   before_action :authenticate
+  before_action :build_course, only: %i[show delete withdraw]
 
   def index
     @provider = Provider.includes(:courses).find(params[:provider_code]).first
   end
 
-  def show
+  def show; end
+
+  def withdraw; end
+
+  def delete; end
+
+private
+
+  def build_course
     @course = Course.where(provider_code: params[:provider_code]).find(params[:code]).first
   end
 end
--- spec/features/courses/show_spec.rb
+++ spec/features/courses/show_spec.rb
@@ -1,29 +1,23 @@
 require 'rails_helper'
 
 feature 'Show course', type: :feature do
-  let(:course) do
-    jsonapi(
-      :course,
-      provider: jsonapi(:provider)
-    ).render
-  end
-  let(:course_attributes) { course[:data][:attributes] }
-
+  let(:course) { jsonapi :course }
+  let(:course_response) { course.render }
   before do
     stub_omniauth
     stub_session_create
     stub_api_v2_request(
-      "/providers/AO/courses/#{course_attributes[:course_code]}",
-      course
+      "/providers/A0/courses/#{course.attributes[:course_code]}",
+      course_response
     )
   end
 
   scenario 'viewing the show courses page' do
-    visit "/organisations/AO/courses/#{course_attributes[:course_code]}"
+    visit "/organisations/A0/courses/#{course.attributes[:course_code]}"
 
-    expect(find('.govuk-caption-xl')).to have_content(course_attributes[:description])
+    expect(find('.govuk-caption-xl')).to have_content(course.attributes[:description])
     expect(find('.govuk-heading-xl')).to have_content(
-      "#{course_attributes[:name]} (#{course_attributes[:course_code]})"
+      "#{course.attributes[:name]} (#{course.attributes[:course_code]})"
     )
   end
 end
