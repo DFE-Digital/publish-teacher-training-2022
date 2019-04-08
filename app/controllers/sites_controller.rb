a789819968fe6c0653c57dc694a172c5ea5ada68
dankmitchell <dan@danmitchell.co.uk>
2019-04-08 08:18:24 +0100

    [1267] Add location edit page

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,8 +1,17 @@
 class SitesController < ApplicationController
-  before_action :authenticate
+  before_action :authenticate, :build_provider
 
   def index
-    @provider = Provider.includes(:sites).find(params[:provider_code]).first
     @sites = @provider.sites.sort_by(&:location_name)
+  end
+
+  def edit
+    @site = Site.where(provider_code: params[:provider_code]).find(params[:id]).first
+  end
+
+private
+
+  def build_provider
+    @provider = Provider.includes(:sites).find(params[:provider_code]).first
   end
 end
