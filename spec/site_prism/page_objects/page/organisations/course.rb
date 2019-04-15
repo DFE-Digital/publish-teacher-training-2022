1992e16c103d799132c39dc02e1e9664d51b7d28
Theodor Vararu <theo@vararu.org>
2019-04-15 14:19:06 +0100

    [1253] Refactor course#show test to use page object

---
+++ spec/site_prism/page_objects/page/organisations/course.rb
@@ -0,0 +1,19 @@
+module PageObjects
+  module Page
+    module Organisations
+      class Course < PageObjects::Base
+        set_url '/organisations/{provider_code}/course/{course_code}'
+
+        element :title, '.govuk-heading-xl'
+        element :caption, '.govuk-caption-xl'
+        element :qualifications, '[data-qa=course__qualifications]'
+        element :study_mode, '[data-qa=course__study_mode]'
+        element :start_date, '[data-qa=course__start_date]'
+        element :name, '[data-qa=course__name]'
+        element :description, '[data-qa=course__description]'
+        element :course_code, '[data-qa=course__course_code]'
+        element :locations, '[data-qa=course__locations]'
+      end
+    end
+  end
+end
