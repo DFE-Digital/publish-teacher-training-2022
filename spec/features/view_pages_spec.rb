e3fe9535ee184bf0b9a96f60beb7a130d8d4f40d
Misha Gorodnitzky <misaka@pobox.com>
2019-04-18 21:52:42 +0100

    [1247] rename transition page to transition_info

--- spec/features/view_pages_spec.rb
+++ spec/features/view_pages_spec.rb
@@ -30,11 +30,11 @@ RSpec.feature 'View pages', type: :feature do
     expect(find('h1')).to have_content('Guidance for Publish teacher training courses')
   end
 
-  scenario "Navigate to /transition" do
+  scenario "Navigate to /transition_info" do
     stub_omniauth
     stub_session_create
 
-    visit "/transition"
+    visit "/transition_info"
     expect(find('h1')).to have_content('Important new features')
     expect(page).to have_link('Continue')
   end
