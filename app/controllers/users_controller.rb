272bcd92ecf669108576568dcf07485d59977dc7
Jake Benilov <benilov@gmail.com>
2019-04-23 17:29:35 +0100

    [1247] Hide some of the client library logic onto User for clarity

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def accept_transition_info
-    user = User.new(id: current_user["user_id"])
-    user.accept_transition_screen
+    User.member(current_user['user_id']).accept_transition_screen
     redirect_to providers_path
   end
 end
