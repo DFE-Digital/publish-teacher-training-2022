5bfe797c6f83ec7c6c6c43f58dce9cdb6dc2033a
Heidar Bernhardsson <heidarbn@gmail.com>
2019-04-24 16:31:42 +0100

    DRY controller authentication and auth by default
    Never again.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,4 @@
 class UsersController < ApplicationController
-  before_action :authenticate
-
   def accept_transition_info
     User.member(current_user['user_id']).accept_transition_screen
     redirect_to providers_path
