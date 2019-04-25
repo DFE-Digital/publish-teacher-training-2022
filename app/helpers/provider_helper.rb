119946e53f4e794b7350af21b8858a8372da37fb
Jake Benilov <benilov@gmail.com>
2019-04-25 17:32:42 +0100

    [1369] Prepopulate email and provider code on new location form

--- app/helpers/provider_helper.rb
+++ app/helpers/provider_helper.rb
@@ -12,4 +12,12 @@ module ProviderHelper
   def add_course_link(email, provider)
     link_to "Add a new course", add_course_url(email, provider), class: "govuk-button govuk-!-margin-bottom-2", rel: "noopener noreferrer", target: "_blank"
   end
+
+  def add_location_url(email, provider)
+    Settings.google_forms.add_location_url + "?" + {usp: 'pp_url', "entry.1913622198" => email, "entry.1075663095" => provider.provider_code}.to_query
+  end
+
+  def add_location_link(email, provider)
+    link_to "Add a location", add_location_url(email, provider), rel: "noopener noreferrer", class: "govuk-button govuk-!-margin-bottom-2", target: :blank
+  end
 end
