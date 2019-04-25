06a39cccaa140abbe070141508794ea88ecb150d
Theodor Vararu <theo@vararu.org>
2019-04-25 16:13:42 +0100

    [1372] Handle client / server errors on accept_transition_info
    Ensure users are always directed to the providers page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,15 @@ class UsersController < ApplicationController
   def accept_transition_info
     User.member(current_user['user_id']).accept_transition_screen
     redirect_to providers_path
+  rescue JsonApiClient::Errors::ClientError, JsonApiClient::Errors::ServerError => e
+    e.env.body.delete("traces")
+    Raven.extra_context(e.env)
+    Raven.capture_exception(e)
+
+    if e.is_a? JsonApiClient::Errors::ClientError
+      redirect_to providers_path
+    else
+      raise e
+    end
   end
 end
