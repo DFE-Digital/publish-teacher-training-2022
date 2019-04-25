4dd11df436f2313ec8c401db062f71d105cb37de
Jake Benilov <benilov@gmail.com>
2019-04-25 18:05:33 +0100

    [1369] Move all the form-specific bits of data into the settings file

--- app/helpers/provider_helper.rb
+++ app/helpers/provider_helper.rb
@@ -1,11 +1,9 @@
 module ProviderHelper
   def add_course_url(email, provider)
     if provider.accredited_body?
-      Settings.google_forms.new_course_for_accredited_bodies_url + "?" +
-        {usp: 'pp_url', "entry.957076544" => email, "entry.1735563938" => provider.provider_code}.to_query
+      google_form_url_for(Settings.google_forms.new_course_for_accredited_bodies, email, provider)
     else
-      Settings.google_forms.new_course_for_unaccredited_bodies_url + "?" +
-        {usp: 'pp_url', "entry.1033530353" => email, "entry.158771972" => provider.provider_code}.to_query
+      google_form_url_for(Settings.google_forms.new_course_for_unaccredited_bodies, email, provider)
     end
   end
 
@@ -14,10 +12,15 @@ module ProviderHelper
   end
 
   def add_location_url(email, provider)
-    Settings.google_forms.add_location_url + "?" + {usp: 'pp_url', "entry.1913622198" => email, "entry.1075663095" => provider.provider_code}.to_query
+    google_form_url_for(Settings.google_forms.add_location, email, provider)
   end
 
   def add_location_link(email, provider)
     link_to "Add a location", add_location_url(email, provider), rel: "noopener noreferrer", class: "govuk-button govuk-!-margin-bottom-2", target: :blank
+  end
+
+  def google_form_url_for(settings, email, provider)
+    settings.url + "&" +
+      { settings.email_entry => email, settings.provider_code_entry => provider.provider_code }.to_query
   end
 end
