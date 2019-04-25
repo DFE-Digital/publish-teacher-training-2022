084ed5e3867b7ee682f3d532429c7c95c047e49f
Jake Benilov <benilov@gmail.com>
2019-04-25 12:03:11 +0100

    [1369] Prepopulate email and provider code on new course form

--- app/helpers/provider_helper.rb
+++ app/helpers/provider_helper.rb
@@ -1,11 +1,15 @@
 module ProviderHelper
-  def add_course_link(provider)
-    google_form_url = if provider.accredited_body?
-                        Settings.google_forms.new_course_for_accredited_bodies_url
-                      else
-                        Settings.google_forms.new_course_for_unaccredited_bodies_url
-                      end
+  def add_course_url(email, provider)
+    if provider.accredited_body?
+      Settings.google_forms.new_course_for_accredited_bodies_url + "?" +
+        {usp: 'pp_url', "entry.957076544" => email, "entry.1735563938" => provider.provider_code}.to_query
+    else
+      Settings.google_forms.new_course_for_unaccredited_bodies_url + "?" +
+        {usp: 'pp_url', "entry.1033530353" => email, "entry.158771972" => provider.provider_code}.to_query
+    end
+  end
 
-    link_to "Add a new course", google_form_url, class: "govuk-button govuk-!-margin-bottom-2", rel: "noopener noreferrer", target: "_blank"
+  def add_course_link(email, provider)
+    link_to "Add a new course", add_course_url(email, provider), class: "govuk-button govuk-!-margin-bottom-2", rel: "noopener noreferrer", target: "_blank"
   end
 end
