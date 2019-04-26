8dcf185aaa0c6ca5f82570f92ecae3f29fa182ea
Misha Gorodnitzky <misaka@pobox.com>
2019-04-26 14:55:18 +0100

    [1370] don't guard against object being nil, exceptions are good!

--- app/decorators/course_decorator.rb
+++ app/decorators/course_decorator.rb
@@ -2,6 +2,6 @@ class CourseDecorator < ApplicationDecorator
   delegate_all
 
   def last_published_at
-    object&.last_published_at&.to_date&.strftime("%-d %B %Y")
+    object.last_published_at&.to_date&.strftime("%-d %B %Y")
   end
 end
