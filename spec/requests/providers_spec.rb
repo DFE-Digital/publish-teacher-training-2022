45c47544752a78093835ecdbd75ab2fb326d9242
dankmitchell <dan@danmitchell.co.uk>
2019-05-03 16:58:28 +0100

    [1431] replace provider controller spec with request spec

--- spec/requests/providers_spec.rb
+++ spec/requests/providers_spec.rb
@@ -3,7 +3,38 @@ require 'rails_helper'
 describe 'Providers', type: :request do
   before do
     stub_omniauth
-    stub_session_create
+    get(auth_dfe_callback_path)
+  end
+
+  describe 'GET index' do
+    context 'with 1 provider' do
+      it 'redirects to providers show' do
+        provider = jsonapi(:provider)
+        stub_api_v2_request('/providers', provider.render)
+        get(providers_path)
+        expect(response).to redirect_to provider_path(provider.provider_code)
+      end
+    end
+
+    context 'with 2 or more providers' do
+      it 'renders providers index' do
+        provider1 = jsonapi(:provider, include_counts: [:courses])
+        provider2 = jsonapi(:provider, include_counts: [:courses])
+        providers = jsonapi(:providers_response, data: [provider1.render[:data], provider2.render[:data]])
+        stub_api_v2_request('/providers', providers)
+        get(providers_path)
+        expect(response.body).to include('Organisations')
+        expect(response.body).to include(provider1.provider_name)
+      end
+    end
+
+    context 'user has no providers' do
+      it 'redirects to manage-courses-ui' do
+        stub_api_v2_request('/providers', jsonapi(:providers_response, data: []))
+        get(providers_path)
+        expect(response).to redirect_to(Settings.manage_ui.base_url)
+      end
+    end
   end
 
   describe 'GET show' do
