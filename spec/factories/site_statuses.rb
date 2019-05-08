efd2db02dd3773f0f7b7a37c173490bd6d3d7a07
Dan Mitchell <dan@danmitchell.co.uk>
2019-05-08 21:06:50 +0100

    [1430] Only display running site locations (#250)
    * [1430] Display site status if not running
    
    * [1430] Move display logic from view into course decorator
    
    * [1430] Tweaks to how location names are displayed
    
    - don't label new site statuses explicitly; we only expect new site
    statuses before the course is published, when all the site statuses are new
    - don't be specific about reason for location not running (discontinued/suspended);
    we aren't using these terms anywhere else in Publish, it's UCAS-specific lingo.

--- spec/factories/site_statuses.rb
+++ spec/factories/site_statuses.rb
@@ -7,6 +7,10 @@ FactoryBot.define do
       vac_status { 'both_full_time_and_part_time_vacancies' }
     end
 
+    trait :full_time do
+      vac_status { 'full_time_vacancies' }
+    end
+
     trait :part_time do
       vac_status { 'part_time_vacancies' }
     end
