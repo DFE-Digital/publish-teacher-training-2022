6cfdd269cc6dba394c03706221b83ef40a4fbd0a
dankmitchell <dan@danmitchell.co.uk>
2019-05-15 12:47:56 +0100

    [1473] Add browser alert if unsaved changes

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -6,3 +6,10 @@ initAll();
 
 var $cookieMessage = document.querySelector('[data-module="cookie-message"]');
 new CookieMessage($cookieMessage).init();
+
+var $copyWarningMessage = document.querySelector('[data-copy-course="warning"]');
+if ($copyWarningMessage) {
+  window.onbeforeunload = function() {
+    return 'You have unsaved changes, are you sure you want to leave?'
+  }
+}
