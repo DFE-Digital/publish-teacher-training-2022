f51dd1ec5710be2839fd55c0ad6cebc855dda938
dankmitchell <dan@danmitchell.co.uk>
2019-05-16 11:31:30 +0100

    [1473] Fix up warning and success summaries

--- spec/requests/courses/about_spec.rb
+++ spec/requests/courses/about_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-describe 'Courses' do
+describe 'Courses', type: :request do
   describe 'GET about' do
     let(:course_json_api)   { jsonapi :course, name: 'English', course_code: 'EN01', provider: provider, include_nulls: [:accrediting_provider] }
     let(:provider)          { jsonapi(:provider, accredited_body?: true, provider_code: 'AO') }
     let(:course)            { course_json_api.to_resource }
     let(:course_response)   { course_json_api.render }
 
-    let(:course_1_json_api)   { jsonapi :course, name: 'English', course_code: 'EN01', include_nulls: [:accrediting_provider] }
+    let(:course_1_json_api) { jsonapi :course, name: 'English', course_code: 'EN01', include_nulls: [:accrediting_provider] }
     let(:course_2_json_api) do
       jsonapi :course,
               name: 'Biology',
@@ -39,18 +39,38 @@ describe 'Courses' do
     end
 
     it 'renders the course about' do
-      get(about_provider_course_path(provider_code: provider.provider_code, code: course.course_code))
-      expect(response.body).to include(course.name)
+      get(about_provider_course_path(provider_code: provider.provider_code,
+                                     code: course.course_code))
+
+      expect(response.body).to include(
+        "#{course.name} (#{course.course_code})"
+      )
+      expect(response.body).to include(
+        'About this course'
+      )
+      expect(response.body).to_not include(
+        'Your changes are not yet saved'
+      )
     end
 
     context 'with copy_form parameter' do
       it 'renders the course about with data from chosen' do
-        get(about_provider_course_path(provider_code: provider.provider_code, code: course.course_code, params: { copy_from: course_2.course_code }))
+        get(about_provider_course_path(provider_code: provider.provider_code,
+                                       code: course.course_code,
+                                       params: { copy_from: course_2.course_code }))
 
-        expect(response.body).to include(course.name)
-        expect(response.body).to include(course_2.about_course)
-        expect(response.body).to include(course_2.interview_process)
-        expect(response.body).to include(course_2.how_school_placements_work)
+        expect(response.body).to include(
+          'Your changes are not yet saved'
+        )
+        expect(response.body).to include(
+          course_2.about_course
+        )
+        expect(response.body).to include(
+          course_2.interview_process
+        )
+        expect(response.body).to include(
+          course_2.how_school_placements_work
+        )
       end
     end
   end
