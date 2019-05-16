c41986d3cf61e13b665fb861d5c8480df5a1276c
dankmitchell <dan@danmitchell.co.uk>
2019-05-16 10:46:51 +0100

    [1473] Fix feature spec for about course

--- spec/features/courses/about_spec.rb
+++ spec/features/courses/about_spec.rb
@@ -1,20 +1,27 @@
 require 'rails_helper'
 
 feature 'About course', type: :feature do
-  let(:course_jsonapi) do
-    jsonapi(
-      :course,
-      provider: jsonapi(:provider, provider_code: 'A0')
-    )
+  let(:course_1) { jsonapi :course, name: 'English', provider: provider, include_nulls: [:accrediting_provider] }
+  let(:course_2) { jsonapi :course, name: 'Biology', include_nulls: [:accrediting_provider] }
+  let(:course_3) { jsonapi :course, name: 'Physics', include_nulls: [:accrediting_provider] }
+  let(:course_4) { jsonapi :course, name: 'Science', include_nulls: [:accrediting_provider] }
+  let(:courses)  { [course_2, course_3, course_4] }
+  let(:provider) do
+    jsonapi(:provider, courses: courses, accredited_body?: true, provider_code: 'AO')
   end
-  let(:course)          { course_jsonapi.to_resource }
-  let(:course_response) { course_jsonapi.render }
+  let(:provider_response) { provider.render }
+  let(:course)            { course_1.to_resource }
+  let(:course_response)   { course_1.render }
 
   before do
     stub_omniauth
     stub_api_v2_request(
       "/providers/AO/courses/#{course.course_code}?include=site_statuses.site,provider.sites,accrediting_provider",
       course_response
+    )
+    stub_api_v2_request(
+      "/providers/AO?include=courses.accrediting_provider",
+      provider_response
     )
   end
 
