d616450f5ca6e05c091b03c89be7622abea20e12
dankmitchell <dan@danmitchell.co.uk>
2019-05-30 14:53:39 +0100

    [1438] dont require line breaks around markdown blocks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def markdown(source)
     render = Govuk::MarkdownRenderer
-    markdown = Redcarpet::Markdown.new(render, extensions = { autolink: true })
+    markdown = Redcarpet::Markdown.new(render, extensions = { autolink: true, lax_spacing: true })
     markdown.render(source).html_safe
   end
 end
