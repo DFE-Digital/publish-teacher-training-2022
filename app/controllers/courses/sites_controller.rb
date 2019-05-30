bc71e9b7aa9d6dfabd77f9dadd97325f1465dca9
Theodor Vararu <theo@vararu.org>
2019-05-30 17:15:40 +0100

    [1513] Hook up edit locations form
    This allows users to add locations to a course.

--- app/controllers/courses/sites_controller.rb
+++ app/controllers/courses/sites_controller.rb
@@ -8,6 +8,28 @@ module Courses
 
     def edit; end
 
+    def update
+      @course.provider_code = @provider.provider_code
+      @course.sites = params.dig(:course, :site_statuses_attributes)
+        &.values
+        &.select { |field| field["selected"] == "1" }
+        &.map { |field| @provider.sites.find { |site| site.id == field["id"] } }
+
+      if @course.save
+        @course.sync_with_search_and_compare(provider_code: params[:provider_code])
+
+        redirect_to provider_course_path(params[:provider_code], params[:code]), flash: { success: 'Course locations published' }
+      else
+        @errors = @site.errors.reduce({}) { |errors, (field, message)|
+          errors[field] ||= []
+          errors[field].push(map_errors(message))
+          errors
+        }
+
+        render :edit
+      end
+    end
+
   private
 
     def build_course
