b3ef815c7c6b86d327a08047e96e39c67db5bcb3
Paul Hayes <fofr@users.noreply.github.com>
2019-06-11 14:55:04 +0100

    Simplify and fix requirements spec

--- spec/features/courses/requirements_spec.rb
+++ spec/features/courses/requirements_spec.rb
@@ -1,37 +1,32 @@
 require 'rails_helper'
 
 feature 'Course requirements', type: :feature do
-  let(:course_1) { jsonapi :course, name: 'English', provider: provider, include_nulls: [:accrediting_provider] }
-  let(:course_2) { jsonapi :course, name: 'Biology', include_nulls: [:accrediting_provider] }
   let(:provider) do
-    jsonapi(:provider, courses: [course_2], accredited_body?: true, provider_code: 'AO')
+    jsonapi(:provider, provider_code: 'AO')
   end
-  let(:provider_response) { provider.render }
-  let(:course)            { course_1.to_resource }
-  let(:course_response)   { course_1.render }
+
+  let(:course) do
+    jsonapi(
+      :course,
+      name: 'English',
+      provider: provider,
+      required_qualifications: 'Required qualifications',
+      personal_qualities: 'Personal qualities',
+      other_requirements: 'Other requirements'
+    )
+  end
 
   before do
     stub_omniauth
-    stub_api_v2_request(
-      "/providers/AO/courses/#{course.course_code}?include=sites,provider.sites,accrediting_provider",
-      course_response
-    )
-    stub_api_v2_request(
-      "/providers/AO?include=courses.accrediting_provider",
-      provider_response
-    )
-    stub_api_v2_request(
-      "/providers/AO/courses/#{course.course_code}",
-      course_response,
-      :patch
-    )
+    stub_course_request(provider, course)
+    stub_api_v2_request("/providers/AO?include=courses.accrediting_provider", provider.render)
+    stub_api_v2_request("/providers/AO/courses/#{course.course_code}", course.render, :patch)
   end
 
   let(:course_requirements_page) { PageObjects::Page::Organisations::CourseRequirements.new }
 
   scenario 'viewing the courses requirements page' do
     visit description_provider_course_path(provider.provider_code, course.course_code)
-
     click_on 'Requirements and eligibility'
 
     expect(current_path).to eq requirements_provider_course_path('AO', course.course_code)
@@ -42,13 +37,13 @@ feature 'Course requirements', type: :feature do
     expect(course_requirements_page.title).to have_content(
       "Requirements and eligibility"
     )
-    expect(course_requirements_page.required_qualifications).to have_content(
+    expect(course_requirements_page.required_qualifications.value).to eq(
       course.required_qualifications
     )
-    expect(course_requirements_page.personal_qualities).to have_content(
+    expect(course_requirements_page.personal_qualities.value).to eq(
       course.personal_qualities
     )
-    expect(course_requirements_page.other_requirements).to have_content(
+    expect(course_requirements_page.other_requirements.value).to eq(
       course.other_requirements
     )
 
@@ -63,5 +58,12 @@ feature 'Course requirements', type: :feature do
     )
 
     expect(current_path).to eq description_provider_course_path('AO', course.course_code)
+  end
+
+  def stub_course_request(provider, course)
+    stub_api_v2_request(
+      "/providers/#{provider.provider_code}/courses/#{course.course_code}?include=sites,provider.sites,accrediting_provider",
+      course.render
+    )
   end
 end
