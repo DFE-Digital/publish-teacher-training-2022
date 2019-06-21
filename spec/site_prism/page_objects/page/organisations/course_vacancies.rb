df931303cca131ed67349026bc15b163275085ad
Paul Hayes <fofr@users.noreply.github.com>
2019-06-21 12:56:22 +0100

    Use SitePrism for vacancy feature specs

---
+++ spec/site_prism/page_objects/page/organisations/course_vacancies.rb
@@ -0,0 +1,16 @@
+module PageObjects
+  module Page
+    module Organisations
+      class CourseVacancies < CourseBase
+        set_url '/organisations/{provider_code}/courses/{course_code}/vacancies'
+
+        element :title, 'h1.govuk-fieldset__heading'
+
+        element :vacancies_radio_choice, '.govuk-radios'
+        element :vacancies_radio_no_vacancies, '#course_has_vacancies_false.govuk-radios__input'
+        element :vacancies_radio_has_some_vacancies, '#course_has_vacancies_true.govuk-radios__input'
+        element :vacancies_running_sites_checkboxes, '.govuk-radios__conditional .govuk-checkboxes'
+      end
+    end
+  end
+end
