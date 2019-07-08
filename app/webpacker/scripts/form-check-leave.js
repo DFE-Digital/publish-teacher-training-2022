de8ac02c290007f0af95e6f877c1a22fa930349b
markmead <itsmarkmead@gmail.com>
2019-07-08 11:24:12 +0100

    Added form check when user leaves

--- app/webpacker/scripts/form-check-leave.js
+++ app/webpacker/scripts/form-check-leave.js
@@ -1,28 +1,36 @@
 function FormCheckLeave($module) {
   this.$module = $module;
-  this.$changed = false;
+  this.$data = "";
 }
+
+FormCheckLeave.prototype.data = function() {
+  const $form = this.$module;
+  return Array.from(new FormData($form), e =>
+    e.map(encodeURIComponent).join("=")
+  ).join("&");
+};
 
 FormCheckLeave.prototype.init = function() {
   const $form = this.$module;
+  let $data = this.$data;
 
   if (!$form) return;
 
-  this.action($form);
+  $data = this.data();
+
+  this.action($form, $data);
 };
 
-FormCheckLeave.prototype.action = function($form) {
-  let $changed = this.$changed;
-  $form.addEventListener("input", () => ($changed = true));
+FormCheckLeave.prototype.action = function($form, $data) {
+  const $changes = this.data();
   $form.addEventListener("submit", () => (window.onbeforeunload = null));
 
   window.onbeforeunload = function() {
-    if ($changed) {
-      event.preventDefault();
-      event.returnValue =
-        "You have unsaved changes, are you sure you want to leave?";
-      return "You have unsaved changes, are you sure you want to leave?";
-    }
+    event.preventDefault();
+    event.preventDefault();
+    event.returnValue =
+      "You have unsaved changes, are you sure you want to leave?";
+    return "You have unsaved changes, are you sure you want to leave?";
   };
 };
 
