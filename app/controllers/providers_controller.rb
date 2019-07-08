df48be34c59bbf9c458b3af7de57c09cfa269076
Paul Hayes <fofr@users.noreply.github.com>
2019-07-08 12:14:56 +0100

    [1394] Link to the next cycle
    - Link to both current and next cycles from the provider
    - Use recruitment cycle model to get titles and links
    - Update content describing each cycle

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -1,4 +1,5 @@
 class ProvidersController < ApplicationController
+  before_action :build_recruitment_cycles, only: %i[show]
   rescue_from JsonApiClient::Errors::NotFound, with: :not_found
 
   def index
@@ -9,5 +10,10 @@ class ProvidersController < ApplicationController
 
   def show
     @provider = Provider.find(params[:code]).first
+  end
+
+  def build_recruitment_cycles
+    @current_recruitment_cycle = RecruitmentCycle.new(Settings.current_cycle)
+    @next_recruitment_cycle = RecruitmentCycle.new(Settings.current_cycle + 1)
   end
 end
