2769bd3c4e1a3d4ee72a1dca480ba622131a3a30
MARK <50486078+markmead@users.noreply.github.com>
2019-07-09 16:17:59 +0100

    [1722] Warn user when they leave unsaved/changed form (#436)
    * setup form prompt when user leaves without saving
    
    * Added form check when user leaves
    
    * tidyup init & onbeforeunload function
    
    * added form module data type to salary
    
    * removed es6 & fixed comparison
    
    * switched to onchange handler
    
    * fix for copy content

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -1,13 +1,19 @@
 import "../stylesheets/application.scss";
 import { initAll } from "govuk-frontend";
 import CookieMessage from "scripts/cookie-banner";
+import FormCheckLeave from "scripts/form-check-leave";
 
 initAll();
 
 var $cookieMessage = document.querySelector('[data-module="cookie-message"]');
 new CookieMessage($cookieMessage).init();
 
-var $copyWarningMessage = document.querySelector('[data-copy-course="warning"]');
+const $form = document.querySelector('[data-module="form-check-leave"]');
+new FormCheckLeave($form).init();
+
+var $copyWarningMessage = document.querySelector(
+  '[data-copy-course="warning"]'
+);
 if ($copyWarningMessage) {
   var $enrichmentForm = document.querySelector('[data-qa="enrichment-form"]');
 
@@ -18,6 +24,6 @@ if ($copyWarningMessage) {
   }
 
   window.onbeforeunload = function() {
-    return 'You have unsaved changes, are you sure you want to leave?'
-  }
+    return "You have unsaved changes, are you sure you want to leave?";
+  };
 }
