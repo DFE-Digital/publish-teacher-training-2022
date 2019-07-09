42e87483b0c9d7f79f1bf9915dd0183a503984e6
markmead <itsmarkmead@gmail.com>
2019-07-09 14:07:57 +0100

    switched to onchange handler

--- app/webpacker/scripts/form-check-leave.js
+++ app/webpacker/scripts/form-check-leave.js
@@ -4,20 +4,21 @@ function FormCheckLeave($module) {
 
 FormCheckLeave.prototype.init = function() {
   const $form = this.$module;
+  let $change = false;
 
   if ($form) {
-    const $originalFormContent = encodeURIComponent(
-      Array.from(new FormData($form))
-    );
+    $form.addEventListener("submit", function() {
+      window.onbeforeunload = null;
+    });
+
+    $form.addEventListener("change", function() {
+      $change = true;
+    });
 
     window.onbeforeunload = function(event) {
-      const $updatedFormContent = encodeURIComponent(
-        Array.from(new FormData($form))
-      );
-
-      // Used to handle browsers that use legacy onbeforeunload
-      // https://developer.mozilla.org/en-US/docs/Web/API/Window/beforeunload_event
-      if ($updatedFormContent !== $originalFormContent) {
+      if ($change) {
+        // Used to handle browsers that use legacy onbeforeunload
+        // https://developer.mozilla.org/en-US/docs/Web/API/Window/beforeunload_event
         event.preventDefault();
         event.returnValue =
           "You have unsaved changes, are you sure you want to leave?";
