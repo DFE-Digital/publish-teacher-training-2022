269b5957603d1b02e9ab1ca638be396bca25d7b2
Paul Hayes <fofr@users.noreply.github.com>
2019-07-19 12:04:41 +0100

    Link to editing entry requirements
    Simplify the basic details tab – the explanatory text can be largely removed as this now lives on the edit screen – before providers needed to know their options before they contacted us.
    
    Use details to give explanatory text for each option.

--- spec/features/courses/entry_requirements/edit_spec.rb
+++ spec/features/courses/entry_requirements/edit_spec.rb
@@ -34,6 +34,19 @@ feature 'Edit course entry requirements', type: :feature do
       )
     end
 
+    scenario 'can cancel changes' do
+      click_on 'Cancel changes'
+      expect(course_details_page).to be_displayed
+    end
+
+    scenario 'can navigate to the edit screen and back again' do
+      course_details_page.load_with_course(course)
+      click_on 'Change entry requirements'
+      expect(entry_requirements_page).to be_displayed
+      click_on 'Back'
+      expect(course_details_page).to be_displayed
+    end
+
     scenario 'presents a choice for each subject' do
       expect(entry_requirements_page).to have_maths_requirements
       expect(entry_requirements_page).to have_english_requirements
