a224b365939f1d95b942a0b363eb644006d72e47
Paul Hayes <fofr@users.noreply.github.com>
2019-07-26 15:20:10 +0100

    [1827] DRY up radio buttons for basic details
    Create a reusable partial that loops over edit options for a particular key, showing labels and help text and selecting the current value.
    
    Also includes logic for showing errors and legends.

--- spec/site_prism/page_objects/page/organisations/course_entry_requirements.rb
+++ spec/site_prism/page_objects/page/organisations/course_entry_requirements.rb
@@ -4,9 +4,9 @@ module PageObjects
       class CourseEntryRequirements < CourseBase
         set_url '/organisations/{provider_code}/{recruitment_cycle_year}/courses/{course_code}/entry-requirements'
 
-        element :maths_requirements,   '[data-qa="course__maths_requirements"]'
-        element :english_requirements, '[data-qa="course__english_requirements"]'
-        element :science_requirements, '[data-qa="course__science_requirements"]'
+        element :maths_requirements,   '[data-qa="course__maths"]'
+        element :english_requirements, '[data-qa="course__english"]'
+        element :science_requirements, '[data-qa="course__science"]'
       end
     end
   end
