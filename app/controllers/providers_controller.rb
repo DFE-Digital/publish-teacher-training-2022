bb12c0b5dbe48406103e3794be0ae149e23541ca
Theodor Vararu <theo@vararu.org>
2019-07-30 12:57:06 +0100

    [1792] Build recruitment cycle in provider controller

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -1,5 +1,6 @@
 class ProvidersController < ApplicationController
   decorates_assigned :provider
+  before_action :build_recruitment_cycle
   rescue_from JsonApiClient::Errors::NotFound, with: :not_found
   before_action :build_provider, except: %i[index show]
 
@@ -42,5 +43,18 @@ private
       .where(year: cycle_year)
       .find(params[:provider_code])
       .first
+  end
+
+  def show_deep_linked_errors(attributes)
+    return if params[:display_errors].blank?
+
+    @provider.publishable?
+    @errors = @provider.errors.messages.select { |key| attributes.include? key }
+  end
+
+  def build_recruitment_cycle
+    cycle_year = params.fetch(:year, Settings.current_cycle)
+
+    @recruitment_cycle = RecruitmentCycle.find(cycle_year).first
   end
 end
