9a59d6ada2f5f398808fcd6dfc8370f0a3677a03
markmead <itsmarkmead@gmail.com>
2019-08-08 11:18:03 +0100

    [1868] show all contact errors on provider editing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -28,12 +28,15 @@ module ApplicationHelper
                 href: href
   end
 
-  def enrichment_summary_label(model, key, field)
-    if @errors&.key? field
+  def enrichment_summary_label(model, key, fields)
+    if fields.select { |field| @errors&.key? field }.any?
+      errors = fields.map { |field|
+        @errors[field]&.map { |error| enrichment_error_link(model, field, error) }
+      }.flatten
       content_tag :dt, class: 'govuk-summary-list__key app-course-parts__fields__label--error' do
         [
           content_tag(:span, key),
-          *@errors[field].map { |error| enrichment_error_link(model, field, error) }
+          *errors
         ].reduce(:+)
       end
     else
@@ -41,7 +44,7 @@ module ApplicationHelper
     end
   end
 
-  def enrichment_summary_value(value, field)
+  def enrichment_summary_value(value, fields)
     css_class = 'govuk-summary-list__value govuk-summary-list__value--truncate'
 
     if value.blank?
@@ -49,12 +52,13 @@ module ApplicationHelper
       css_class += ' app-course-parts__fields__value--empty'
     end
 
-    content_tag :dd, value, class: css_class, data: { qa: "enrichment__#{field}" }
+    data_qa = "enrichment__#{fields.first}"
+    content_tag :dd, value, class: css_class, data: { qa: data_qa }
   end
 
-  def enrichment_summary_item(model, key, value, field)
+  def enrichment_summary_item(model, key, value, fields)
     content_tag :div, class: 'govuk-summary-list__row' do
-      enrichment_summary_label(model, key, field) + enrichment_summary_value(value, field)
+      enrichment_summary_label(model, key, fields) + enrichment_summary_value(value, fields)
     end
   end
 end
