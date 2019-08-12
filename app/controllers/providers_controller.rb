ac24436acfb1029692792e34d6c38b3150abb354
Theodor Vararu <theo@vararu.org>
2019-08-12 11:42:11 +0100

    [1926] Fix link for accredited bodies validation error link

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -75,7 +75,7 @@ private
       :address4,
       :postcode,
       :region_code,
-      accredited_bodies: %i[provider_code description]
+      accredited_bodies: %i[provider_name provider_code description]
     ).to_h # Without this, accredited_bodies is an array of params objects
            # instead of an array of plain hashes and gets serialized incorrectly
            # on its way to the backend.
