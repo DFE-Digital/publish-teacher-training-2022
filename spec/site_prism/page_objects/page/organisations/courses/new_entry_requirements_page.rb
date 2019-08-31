91bb6c426df9eb3582aa85e369e707dce4589419
dankmitchell <dan@danmitchell.co.uk>
2019-08-31 15:21:43 +0100

    [2041] Ensure new course routes match edit routes

--- spec/site_prism/page_objects/page/organisations/courses/new_entry_requirements_page.rb
+++ spec/site_prism/page_objects/page/organisations/courses/new_entry_requirements_page.rb
@@ -3,7 +3,7 @@ module PageObjects
     module Organisations
       module Courses
         class NewEntryRequirementsPage < CourseBase
-          set_url '/organisations/{provider_code}/{recruitment_cycle_year}/courses/entry_requirements/new'
+          set_url '/organisations/{provider_code}/{recruitment_cycle_year}/courses/entry-requirements/new'
 
           element :maths_requirements,   '[data-qa="course__maths"]'
           element :english_requirements, '[data-qa="course__english"]'
