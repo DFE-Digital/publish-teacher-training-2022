a1762c8157731ee5e37f5626e7c97451dc6dcd0d
defong <de_fong2k@yahoo.com>
2019-09-04 20:24:54 +0000

    [2036] Fix webpack dev server https key paths
    This fixes the slow running tests we're seeing in CI.

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -5,8 +5,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
 
 environment.config.devServer.https = {
-  key: fs.readFileSync('./config/localhost/https/localhost.key'),
-  cert: fs.readFileSync('./config/localhost/https/localhost.crt')
+  key: './config/localhost/https/localhost.key',
+  cert: './config/localhost/https/localhost.crt'
 }
 
 module.exports = environment.toWebpackConfig()
