f1e519f1e9a1af5cabed0ac9f30b30b2fed82ff6
Misha Gorodnitzky <misaka@pobox.com>
2019-11-28 23:34:10 +0000

    [1625] clean-ups

--- cypress/cypress/integration/example.js
+++ cypress/cypress/integration/example.js
@@ -27,10 +27,9 @@ describe("login", function () {
 
     cy.clearCookies()
 
-    debugger;
-
     return cy.request(step1_option)
       .then((resp) => {
+        console.log(`GET ${resp.redirectedToUrl}`);
         return cy.request({
           method: 'GET',
           url: resp.redirectedToUrl,
@@ -38,79 +37,72 @@ describe("login", function () {
         });
       })
       .then(response => {
-        var postURL = new URL(
+        var authURL = new URL(
           response.allRequestResponses[response.allRequestResponses.length - 1]["Request URL"]
         );
-        console.log(postURL);
-        // cy.request({
-        //   method: 'POST',
-        //   url: '/login',
-        //   failOnStatusCode: false, // dont fail so we can make assertions
-        //   form: true, // we are submitting a regular form body
-        //   body: {
-        //     username,
-        //     password,
-        //     _csrf: csrfToken // insert this as part of form body
-        //   }
-        // })
 
+        const html = Cypress.$(response.body);
+        // const csrfToken = $html.find("input[name=_csrf]").val();
+        // const clientId = $html.find("input[name=clientId]").val();
+        // const redirectUri = $html.find("input[name=redirectUri]").val();
+        var formInputs = getMapOfFormInputs(html.find('input'));
+        formInputs.username = username;
+        formInputs.password = password;
 
-        const $html = Cypress.$(response.body);
-        const csrfToken = $html.find("input[name=_csrf]").val();
-        const clientId = $html.find("input[name=clientId]").val();
-        const redirectUri = $html.find("input[name=redirectUri]").val();
         const responses = response["allRequestResponses"];
-        const cookiesSet = responses[responses.length - 1]["Response Headers"]["set-cookie"];
-        // 0: "_csrf=KDonEKj24XFEwGcOE8ndYfRr; Path=/; HttpOnly; Secure"
-        // 1: "session=eyJyZWRpcmVjdFVyaSI6bnVsbH0=; path=/; secure; httponly"
-        // 2: "session.sig=SaZR0Tt8DIt3oKMJyJ8qA58_iBw; path=/; secure; httponly"
-        const cookieWholeRE = new RegExp("^([^;]+);");
-        var cookieHeaders = [];
-        if(cookiesSet != undefined) {
-          cookieHeaders = cookiesSet.map((cookie) => { return cookieWholeRE.exec(cookie)[1]; });
-          const cookiePartsRE = new RegExp("^(.+?)=(.+?);");
-          cookiesSet.forEach((cookie) => {
-            var matches = cookiePartsRE.exec(cookie);
-            console.log("Setting cookie: " + matches[1]);
-            cy.setCookie(
-              matches[1],
-              matches[2],
-              {
-                domain: postURL.hostname,
-              }
-            );
-          });
-        } else {
-          console.log("cookies not set: " + cookiesSet);
+        const setCookiesHeaders = responses[responses.length - 1]["Response Headers"]["set-cookie"];
+        if(setCookiesHeaders != undefined) {
+          setCookiesFromHeaders(setCookiesHeaders, authURL.hostname);
         }
 
+        console.log("  get auth response:");
         logger(response);
-
+        console.log("  login page formInputs:");
+        logger(formInputs);
         debugger;
+        // console.log(`curl -b '${cookieHeaders.join("; ")}' -d username=${username} -d password='${password} -d _csrf='${csrfToken} -d cliendId='${clientId} -d redirectUri='${redirectUri}' '${authURL}'`);
 
-        console.log(`curl -b '${cookieHeaders.join("; ")}' -d username=${username} -d password='${password} -d _csrf='${csrfToken} -d cliendId='${clientId} -d redirectUri='${redirectUri}' '${postURL}'`);
-
-        cy.request({
+        console.log(`POST ${authURL.toString()}`);
+        return cy.request({
           method: 'POST',
-          url: postURL.toString(),
+          url: authURL.toString(),
           failOnStatusCode: false,
           form: true,
-          body: {
-            username: username,
-            password: password,
-            _csrf: csrfToken,
-            clientId: clientId,
-            redirectUri: redirectUri,
-          },
-        }).then((response) => {
-            logger(response);
-            debugger;
-          });
+          body: formInputs,
+          //   username: username,
+          //   password: password,
+          //   _csrf: csrfToken,
+          //   clientId: clientId,
+          //   redirectUri: redirectUri,
+          // },
+        });
+      }).then((response) => {
+        console.log("login post response:");
+        logger(response);
+        var continueParams = {};
+        const html = Cypress.$(response.body);
 
+        var formInputs = getMapOfFormInputs(html.find('input'));
+        // $html.find("input").map((idx, input) => {
+        //   continueParams[input.name] = input.value;
+        //   console.log(`${input.name}: ${input.value}`);
+        // });
+        var continueURL = html.find("form")[0].action;
+        debugger;
+
+        console.log(`POST ${continueURL.toString()}`);
+        return cy.request({
+          method: 'POST',
+          url: continueURL.toString(),
+          failOnStatusCode: true,
+          form: true,
+          body: formInputs,
+        });
+      }).then((response => { console.log; }));
   });
 
   it("logging in ", function () {
-    cy.loginWithSignIn("tim.abell+4@digital.education.gov.uk", 'omgsquirrel!88')
+    cy.loginWithSignIn("tim.abell+4@digital.education.gov.uk", 'omgsquirrel!88');
     //   .then((resp) => {
     //     debugger;
     //     expect(resp.status).to.eq(200);
@@ -123,5 +115,36 @@ describe("login", function () {
 
 function logger(thing) {
   console.log(JSON.parse(JSON.stringify(thing)));
+}
+
+// Convert inputs into a mapping of input name to value for convenience.
+//
+// The "inputs" param is the input nodes from the HTML dom, e.g.
+// $html.find('input')
+function getMapOfFormInputs(inputs) {
+  return Object.fromEntries(inputs.map((idx, input) => {
+    return [[input.name, input.value]];
+  }));
+}
+
+function setCookiesFromHeaders(setCookiesHeaders, domain) {
+  // setCookiesHeaders is a list of the set-cookie headers in the response:
+  // 0: "_csrf=KDonEKj24XFEwGcOE8ndYfRr; Path=/; HttpOnly; Secure"
+  // 1: "session=eyJyZWRpcmVjdFVyaSI6bnVsbH0=; path=/; secure; httponly"
+  // 2: "session.sig=SaZR0Tt8DIt3oKMJyJ8qA58_iBw; path=/; secure; httponly"
+
+  // const cookieWholeRE = new RegExp("^([^;]+);");
+  // const cookieSettings = setCookiesHeader.map((cookie) => { return cookieWholeRE.exec(cookie)[1]; });
+
+  const cookiePartsRE = new RegExp("^(.+?)=(.+?);");
+  setCookiesHeaders.forEach((setCookieHeader) => {
+    var matches = cookiePartsRE.exec(setCookieHeader);
+    console.log("Setting cookie: " + matches[1]);
+    cy.setCookie(
+      matches[1],
+      matches[2],
+      { domain: domain }
+    );
+  });
 }
 
