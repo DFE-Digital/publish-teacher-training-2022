3cf59ae1ed061042618f803334d077537e2deb97
defong <de_fong2k@yahoo.com>
2019-12-04 12:29:28 +0000

    [1625] Fixed directory structure

--- e2e/cypress/integration/organisations.js
+++ e2e/cypress/integration/organisations.js
@@ -6,17 +6,7 @@ describe("login", function () {
       .visit(baseUrl);
 
     cy.url().should('eq', baseUrl);
-    cy.get('footer').scrollIntoView({ duration: 2000 });
     cy.get('h1').contains('Organisations');
-  });
-
-  it("viewing provider page ", function () {
-    const url = `${baseUrl}/organisations/T92`;
-    cy.signIn()
-      .visit(url);
-
-    cy.url().should('eq', url);
     cy.get('footer').scrollIntoView({ duration: 2000 });
-    // cy.get('h1').contains('provider name');
   });
 });
