8b2ed2e758f4586fd78fc711446a428bf1cff33c
defong <de_fong2k@yahoo.com>
2019-12-04 09:36:51 +0000

    [1625] Amended extracted sign in command to its own

--- cypress/cypress/plugins/index.js
+++ cypress/cypress/plugins/index.js
@@ -11,6 +11,9 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+// NOTE;  Probably uses `cypress` `bundled` nodejs NOT local machine copy
+//        Therefore needs to be strict vanilla nodejs.
+
 // promisified fs module
 const fs = require('fs-extra');
 const path = require('path');
