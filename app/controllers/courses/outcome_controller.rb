e900d927fdb9155259fcbbe44251bb7d120e5a9c
Daniel Burnley <dan@danops.uk>
2019-12-10 10:55:57 +0000

    [2614] Assign ordered edit options to qualificaitons

--- app/controllers/courses/outcome_controller.rb
+++ app/controllers/courses/outcome_controller.rb
@@ -1,8 +1,17 @@
 module Courses
   class OutcomeController < ApplicationController
     include CourseBasicDetailConcern
+    before_action :order_edit_options, only: %i[edit new]
 
   private
+
+    def order_edit_options
+      @course.meta["edit_options"]["qualifications"] = if @course.level == "further_education"
+                                                         %w[pgce pgde]
+                                                       else
+                                                         %w[pgce_with_qts qts pgde_with_qts]
+                                                       end
+    end
 
     def current_step
       :outcome
