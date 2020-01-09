fcf1f87e5bf14e1d5e48490e0e10509e772d46c0
David Gisbey <davidgisbey@gmail.com>
2020-01-09 13:49:32 +0000

    [1591] Hide feature from non admin users

--- spec/features/providers/show_spec.rb
+++ spec/features/providers/show_spec.rb
@@ -3,11 +3,14 @@ require "rails_helper"
 feature "Show providers", type: :feature do
   let(:organisation_show_page) { PageObjects::Page::Organisations::OrganisationShow.new }
   let(:provider) { build :provider }
+  let(:user) { build :user }
+  let(:access_request) { build :access_request }
 
   before do
-    stub_omniauth
+    stub_omniauth(user: user)
     stub_api_v2_resource(provider)
     stub_api_v2_resource(provider.recruitment_cycle)
+    stub_api_v2_resource_collection([access_request])
   end
 
   context "When the provider is not accredited" do
@@ -17,13 +20,23 @@ feature "Show providers", type: :feature do
     end
   end
 
-  context "When the provider is not accredited" do
+  context "When the provider is accredited" do
     let(:provider) { build :provider, accredited_body?: true }
 
-    it "does not have show the courses as an accredited body link" do
-      visit provider_path(provider.provider_code)
+    context "and the user is not an admin"  do
+      it "does not have show the courses as an accredited body link" do
+        visit provider_path(provider.provider_code)
+        expect { organisation_show_page.courses_as_accredited_body_link }.to raise_error(Capybara::ElementNotFound)
+      end
+    end
 
-      expect(organisation_show_page.courses_as_accredited_body_link.text).to eq("Courses as an accredited body")
+    context "and the user is an admin" do
+      let(:user) { build :user, :admin }
+
+      it "does shows the courses as an accredited body link" do
+        visit provider_path(provider.provider_code)
+        expect(organisation_show_page.courses_as_accredited_body_link.text).to eq("Courses as an accredited body")
+      end
     end
   end
 end
