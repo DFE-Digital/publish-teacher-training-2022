3b97bb81b59c2a1009c66f049247fdc71a09f494
Graham Pengelly <graham@three-tier.com>
2020-01-09 12:14:46 +0000

    [2537] Require login on/providers/suggest
    Attempts to visit `/providers/suggest` whilst unauthenticated resulted
    in a 500 rather than redirecting to signin.
    
    Require authentication in the controller to remedy this and redirect the
    user appropriately.

--- app/controllers/provider_suggestions_controller.rb
+++ app/controllers/provider_suggestions_controller.rb
@@ -1,6 +1,4 @@
 class ProviderSuggestionsController < ApplicationController
-  skip_before_action :authenticate
-
   def suggest
     suggestions = ProviderSuggestion.suggest(params[:query])
       .map { |provider| { code: provider.provider_code, name: provider.provider_name } }
