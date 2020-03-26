4f017c633d06fab5151cff75bd49f2efd82aaf30
Alistair Laing <aliuk2012@users.noreply.github.com>
2020-03-26 17:36:42 +0000

    [3192] Remove region select so users cannot edit
    Removing this form field as it is overriden by geocode lookup.
    It is no longer needed.

--- spec/features/sites/new_spec.rb
+++ spec/features/sites/new_spec.rb
@@ -50,7 +50,7 @@ feature "Locations", type: :feature do
       fill_in "Town or city", with: "New town"
       fill_in "County", with: "New county"
       fill_in "Postcode", with: "SW1A 1AA"
-      select "London", from: "Region of UK"
+
 
       click_on "Save"
 
