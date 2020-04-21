9d90e160af790082fb57a89f3928f5821da25599
Aga Dufrat <aga.dufrat@digital.education.gov.uk>
2020-04-21 16:49:16 +0100

    [3286] Refactor allocations spec
    Rename training_provider to provider, which is a provider that is not an
    accredited body. Training provider will be used in reference to training
    providers that have courses accredited by the accredited body. Move the api
    request stub to where the training provider is build so the
    click_request_pe_courses method can be reused.

--- spec/features/allocations_spec.rb
+++ spec/features/allocations_spec.rb
@@ -21,7 +21,7 @@ RSpec.feature "PE allocations" do
   end
 
   scenario "There is no PE allocations page for non accredited body" do
-    given_training_provider_exists
+    given_a_provider_exists
     given_i_am_signed_in_as_an_admin
 
     when_i_visit_training_providers_page
@@ -52,7 +52,16 @@ RSpec.feature "PE allocations" do
   end
 
   def given_training_provider_with_pe_fee_founded_course_exists
-    @training_provider1 = build(:provider)
+    @training_provider = build(:provider)
+    stub_api_v2_request(
+      "/recruitment_cycles/#{@accrediting_body.recruitment_cycle.year}/providers/" \
+      "#{@accrediting_body.provider_code}/training_providers" \
+      "?filter[funding_type]=fee" \
+      "&filter[subjects]=C6" \
+      "&recruitment_cycle_year=#{@accrediting_body.recruitment_cycle.year}",
+      resource_list_to_jsonapi([@training_provider]),
+    )
+  end
   end
 
   def when_i_visit_my_organisations_page
@@ -64,14 +73,6 @@ RSpec.feature "PE allocations" do
   end
 
   def and_i_click_request_pe_courses
-    stub_api_v2_request(
-      "/recruitment_cycles/#{@accrediting_body.recruitment_cycle.year}/providers/" \
-      "#{@accrediting_body.provider_code}/training_providers" \
-      "?filter[funding_type]=fee" \
-      "&filter[subjects]=C6" \
-      "&recruitment_cycle_year=#{@accrediting_body.recruitment_cycle.year}",
-      resource_list_to_jsonapi([@training_provider1]),
-    )
     click_on "Request PE courses for 2021/22"
   end
 
@@ -80,7 +81,7 @@ RSpec.feature "PE allocations" do
   end
 
   def and_i_see_only_see_training_providers_offering_pe_fee_founded_courses
-    expect(allocations_page.rows.first.provider_name.text).to eq(@training_provider1.provider_name)
+    expect(allocations_page.rows.first.provider_name.text).to eq(@training_provider.provider_name)
   end
 
   def and_i_see_allocations_with_status_and_actions
@@ -97,9 +98,9 @@ RSpec.feature "PE allocations" do
     end
   end
 
-  def given_training_provider_exists
-    @training_provider = build(:provider)
-    stub_api_v2_resource(@training_provider.recruitment_cycle)
+  def given_a_provider_exists
+    @provider = build(:provider)
+    stub_api_v2_resource(@provider.recruitment_cycle)
   end
 
   def there_is_no_request_pe_courses_link
@@ -107,16 +108,16 @@ RSpec.feature "PE allocations" do
   end
 
   def when_i_visit_training_providers_page
-    stub_api_v2_resource(@training_provider)
+    stub_api_v2_resource(@provider)
     stub_api_v2_resource_collection([build(:access_request)])
 
-    visit provider_path(@training_provider.provider_code)
-    expect(find("h1")).to have_content(@training_provider.provider_name.to_s)
+    visit provider_path(@provider.provider_code)
+    expect(find("h1")).to have_content(@provider.provider_name.to_s)
   end
 
   def and_i_cannot_access_pe_alloacations_page
-    allocations_page.load(provider_code: @training_provider.provider_code,
-                          recruitment_cycle_year: @training_provider.recruitment_cycle.year)
+    allocations_page.load(provider_code: @provider.provider_code,
+                          recruitment_cycle_year: @provider.recruitment_cycle.year)
     expect(page).to have_content("Page not found")
   end
 
