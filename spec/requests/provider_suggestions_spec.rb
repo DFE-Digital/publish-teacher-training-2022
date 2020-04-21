425c1975cb5a88db72f1d88cc64ba0891ea8311c
SamJamCul <samuel.culley@digital.education.gov.uk>
2020-04-21 14:42:43 +0100

    [3276] Move UTF-8 encode query to controller

---
+++ spec/requests/provider_suggestions_spec.rb
@@ -0,0 +1,58 @@
+require "rails_helper"
+
+describe "/providers/suggest", type: :request do
+  let(:provider) { build(:provider) }
+
+  before do
+    stub_omniauth(provider: provider)
+    get(auth_dfe_callback_path)
+  end
+
+  context "when provider suggestion is blank" do
+    it "returns bad request (400)" do
+      get "/providers/suggest"
+
+      expect(response.status).to eq(400)
+      expect(JSON.parse(response.body)).to eq("error" => "Bad request")
+    end
+  end
+  context "when provider suggestion is less than three characters" do
+    it "returns bad request (400)" do
+      get "/providers/suggest?query=St"
+
+      expect(response.status).to eq(400)
+      expect(JSON.parse(response.body)).to eq("error" => "Bad request")
+    end
+  end
+  context "when provider suggestion query is valid" do
+    query = "Girls School"
+    query_with_unicode_character = "Girls%E2%80%99 School"
+
+    [query, query_with_unicode_character].each do |provider_query|
+      it "returns success (200) for query: '#{provider_query}'" do
+        provider_suggestions = stub_request(:get, "#{Settings.manage_backend.base_url}/api/v2/providers/suggest?query=#{provider_query}")
+                                 .to_return(
+                                   headers: { "Content-Type": "application/vnd.api+json; charset=utf-8" },
+                                   body: File.new("spec/fixtures/api_responses/provider-suggestions.json"),
+                                 )
+
+        get "/providers/suggest?query=#{provider_query}"
+
+        expect(provider_suggestions).to have_been_requested
+        expect(response.status).to eq(200)
+        expect(JSON.parse(response.body)).to eq(
+          [
+            {
+              "code" => "A0",
+              "name" => "ACME SCITT 0",
+            },
+            {
+              "code" => "A01",
+              "name" => "Acme SCITT",
+            },
+          ],
+         )
+      end
+    end
+  end
+end
