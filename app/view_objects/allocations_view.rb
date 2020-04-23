57ec2e0b382623a80472252c4bd970b621e8fdae
Toby Retallick <toby.retallick@gmail.com>
2020-04-23 12:40:49 +0100

    [3256] - Refactor AllocationsView

--- app/view_objects/allocations_view.rb
+++ app/view_objects/allocations_view.rb
@@ -18,30 +18,40 @@ class AllocationsView
 
   def allocation_statuses
     @training_providers.map do |training_provider|
-      allocation_view = {
-        provider_name: training_provider.provider_name,
-        provider_code: training_provider.provider_code,
-      }
-      matching_allocation = @allocations.select do |allocation|
-        allocation.provider_id == training_provider.id
-      end.first
-
-      if matching_allocation.nil?
-        allocation_view[:status] = Status::YET_TO_REQUEST
-        allocation_view[:status_colour] = Colour::GREY
-      end
-
-      if matching_allocation && matching_allocation[:number_of_places] >= 1
-        allocation_view[:status] = Status::REQUESTED
-        allocation_view[:status_colour] = Colour::GREEN
-      end
-
-      if matching_allocation && matching_allocation[:number_of_places].zero?
-        allocation_view[:status] = Status::NOT_REQUESTED
-        allocation_view[:status_colour] = Colour::RED
-      end
-
-      allocation_view
+      matching_allocation = find_matching_allocation(training_provider)
+      build_allocation_status(matching_allocation, training_provider)
     end
+  end
+
+private
+
+  def find_matching_allocation(training_provider)
+    @allocations.find do |allocation|
+      allocation.provider_id == training_provider.id
+    end
+  end
+
+  def build_allocation_status(matching_allocation, training_provider)
+    allocation_status = {
+      provider_name: training_provider.provider_name,
+      provider_code: training_provider.provider_code,
+    }
+
+    if matching_allocation.nil?
+      allocation_status[:status] = Status::YET_TO_REQUEST
+      allocation_status[:status_colour] = Colour::GREY
+    end
+
+    if matching_allocation && matching_allocation[:number_of_places] >= 1
+      allocation_status[:status] = Status::REQUESTED
+      allocation_status[:status_colour] = Colour::GREEN
+    end
+
+    if matching_allocation && matching_allocation[:number_of_places].zero?
+      allocation_status[:status] = Status::NOT_REQUESTED
+      allocation_status[:status_colour] = Colour::RED
+    end
+
+    allocation_status
   end
 end
