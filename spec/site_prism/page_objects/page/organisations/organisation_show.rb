21b6222f44cf822fd8c4efb887e0a6202da4c09a
Alistair Laing <aliuk2012@users.noreply.github.com>
2020-04-29 09:43:40 +0100

    should be using hyphens for frontend markup

--- spec/site_prism/page_objects/page/organisations/organisation_show.rb
+++ spec/site_prism/page_objects/page/organisations/organisation_show.rb
@@ -4,8 +4,8 @@ module PageObjects
       class OrganisationShow < CourseBase
         set_url "/organisations/{provider_code}/"
 
-        element :courses_as_accredited_body_link, "[data-qa=courses_as_accredited_body_link]"
-        element :request_allocations_link, "[data-qa=request_allocations_link]"
+        element :courses_as_accredited_body_link, "[data-qa=courses-as-accredited-body-link]"
+        element :request_allocations_link, "[data-qa=request-allocations-link]"
         element :notifications_preference_link, "[data-qa='notifications-link']"
       end
     end
