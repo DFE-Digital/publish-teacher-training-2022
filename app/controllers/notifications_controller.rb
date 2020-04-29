a7a6e55db85b3aaa1a058bbe6fa77381b25e9fc5
Alistair Laing <aliuk2012@users.noreply.github.com>
2020-04-29 14:01:27 +0100

    [3293] Basic form POST for notifications
    This is a placeholder for when the API is ready and we can start processing
    the notifications.

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,3 +1,27 @@
 class NotificationsController < ApplicationController
   def index; end
+
+  def create
+    if params[:consent].nil?
+      # TODO: Better Error messages once wired up to API
+      flash[:error] = "Please select one option"
+      redirect_to notifications_path
+      return
+    end
+
+    if params.require(:consent).present?
+      flash[:success] = "Your notification preferences have been saved."
+      redirect_to redirect_to_path
+    end
+  end
+
+private
+
+  def redirect_to_path
+    if params[:provider_code].present?
+      provider_path(params[:provider_code])
+    else
+      root_path
+    end
+  end
 end
