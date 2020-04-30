4760a51ffa1b456a66dc0e6f80be24d6641e670f
Graham Pengelly <graham@three-tier.com>
2020-04-30 16:41:36 +0100

    [3331] - Add `AllocationServices::Create`
    Service to create an `Allocation`.
    
    There are some moderately complex combinations of parameters to handle
    when creating an `Allocation`. This service handles those to simplify
    the controllers and testing.

--- app/models/allocation.rb
+++ app/models/allocation.rb
@@ -1,7 +1,14 @@
 class Allocation < Base
+  module RequestTypes
+    INITIAL = "initial".freeze
+    REPEAT = "repeat".freeze
+    DECLINED = "declined".freeze
+  end
+
   belongs_to :provider, param: :provider_code
 
   property :number_of_places
+  property :request_type
 
   def has_places?
     number_of_places.positive?
---
+++ spec/services/allocation_services/create_spec.rb
@@ -0,0 +1,66 @@
+require "rails_helper"
+
+module AllocationServices
+  describe Create do
+    describe ".call" do
+      let(:provider_id) { 1 }
+      let(:accredited_body_code) { "ABC" }
+      let(:number_of_places) { 10 }
+
+      context "requested: true" do
+        context "number_of_places provided" do
+          it "creates an initial request" do
+            expect(Allocation).to receive(:create)
+              .with(
+                provider_code: accredited_body_code,
+                provider_id: provider_id,
+                number_of_places: number_of_places,
+                request_type: Allocation::RequestTypes::INITIAL,
+              )
+
+            described_class.call(
+              requested: true,
+              accredited_body_code: accredited_body_code,
+              provider_id: provider_id,
+              number_of_places: number_of_places,
+            )
+          end
+        end
+
+        context "number_of_places not provided" do
+          it "creates a repeat request" do
+            expect(Allocation).to receive(:create)
+              .with(
+                provider_code: accredited_body_code,
+                provider_id: provider_id,
+                request_type: Allocation::RequestTypes::REPEAT,
+              )
+
+            described_class.call(
+              requested: true,
+              accredited_body_code: accredited_body_code,
+              provider_id: provider_id,
+            )
+          end
+        end
+      end
+
+      context "requested: false" do
+        it "creates a declined request" do
+          expect(Allocation).to receive(:create)
+            .with(
+              provider_code: accredited_body_code,
+              provider_id: provider_id,
+              request_type: Allocation::RequestTypes::DECLINED,
+            )
+
+          described_class.call(
+            requested: false,
+            accredited_body_code: accredited_body_code,
+            provider_id: provider_id,
+          )
+        end
+      end
+    end
+  end
+end
