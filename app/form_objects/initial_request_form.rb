ea6983f4511c8103dee11cc050ee5b1cdb024fd1
Alistair Laing <aliuk2012@users.noreply.github.com>
2020-05-11 08:46:34 +0100

    Update email  to use global Settings
    Co-authored-by: Aga Dufrat <38078064+AgaDufrat@users.noreply.github.com>

--- app/form_objects/initial_request_form.rb
+++ app/form_objects/initial_request_form.rb
@@ -4,11 +4,9 @@ class InitialRequestForm
   attr_accessor :training_provider_code, :training_provider_query
 
   def add_no_results_error
-    support_email = "becomingateacher@digital.education.gov.uk"
-
     errors.add(:training_provider_query,
                "We couldn't find this organisation - please check your information and try again.
-                To add a new organisation to Publish, contact #{support_email}.")
+                To add a new organisation to Publish, contact #{Settings.service_support.contact_email_address}.")
   end
 
   def add_no_search_query_error
