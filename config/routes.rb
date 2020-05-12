ef57899c44b5de9523eb8538572297e671a13b61
Alistair Laing <aliuk2012@users.noreply.github.com>
2020-05-12 07:31:24 +0100

    Fix lint issue identified in routes.rb
    config/routes.rb:19:105: C: Style/BlockDelimiters: Prefer {...} over do...end
    for single-line blocks. (https://rubystyle.guide#single-line-blocks)
      resources :notifications, path: "/notifications", controller: "notifications",
        only: %i[index create] do; end

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   root to: "providers#index"
   get "/organisations", to: redirect("/")
 
-  resources :notifications, path: "/notifications", controller: "notifications", only: %i[index create] do; end
+  resources :notifications, path: "/notifications", controller: "notifications", only: %i[index create]
 
   resources :access_requests, path: "/access-requests", controller: "access_requests", only: %i[new index create] do
     member do
