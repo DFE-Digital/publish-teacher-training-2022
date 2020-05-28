da70024a9e6fe1c7b6ccd7d4b79d6178f35feb00
Alistair Laing <aliuk2012@users.noreply.github.com>
2020-05-28 15:58:21 +0100

    [3454] Add validations to repeat allocations
    Fixes an 500 error a user would get if they did not select yes/no.
    This commit also removed the "allocation" namespace we added awhile ago.

--- spec/site_prism/page_objects/page/providers/allocations/new_page.rb
+++ spec/site_prism/page_objects/page/providers/allocations/new_page.rb
@@ -6,8 +6,8 @@ module PageObjects
           set_url "/organisations/{provider_code}/{recruitment_cycle_year}/allocations/{training_provider_code}/new"
 
           element :form, "form"
-          element :yes, "#allocation-request-type-repeat-field"
-          element :no, "#allocation-request-type-declined-field"
+          element :yes, "#request-type-repeat-field"
+          element :no, "#request-type-declined-field"
           element :continue_button, "input[value='Continue']"
         end
       end
