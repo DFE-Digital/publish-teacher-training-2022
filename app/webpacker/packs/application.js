fdda476f0e3f8586e0eb146400c33042fbe9f0b9
Alistair Laing <aliuk2012@users.noreply.github.com>
2020-06-12 20:42:57 +0100

    [3535] Remove obsolete javascript
    As we are no longer loading in GA, none of this code would ever be run.

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -5,7 +5,6 @@ import "../stylesheets/application.scss";
 import "accessible-autocomplete/dist/accessible-autocomplete.min.css";
 import { initAll } from "govuk-frontend";
 import FormCheckLeave from "scripts/form-check-leave";
-import { triggerFormAnalytics } from "scripts/form-error-tracking";
 import initLocationsMap from "scripts/locations-map";
 
 initAll();
@@ -30,33 +29,6 @@ if ($copyWarningMessage) {
   window.onbeforeunload = function() {
     return "You have unsaved changes, are you sure you want to leave?";
   };
-}
-
-if (typeof ga === "function") {
-  const $formErrorSummary = document.querySelector(
-    '[data-ga-event-form="error"]'
-  );
-  const $formSuccessSummary = document.querySelector(
-    '[data-ga-event-form="success"]'
-  );
-
-  if ($formErrorSummary) {
-    const $formErrorMessages = $formErrorSummary.querySelectorAll(
-      "[data-error-message]"
-    );
-
-    triggerFormAnalytics("form", "form-submitted", "form-error");
-
-    $formErrorMessages.forEach(function($errorMessage) {
-      triggerFormAnalytics(
-        "form",
-        "form-error-message",
-        $errorMessage.getAttribute("data-error-message")
-      );
-    });
-  } else if ($formSuccessSummary) {
-    triggerFormAnalytics("form", "form-submitted", "form-success");
-  }
 }
 
 try {
