aa08167083c1761a4a7f2157fbfc8b9e0edd0c4b
Misha Gorodnitzky <misaka@pobox.com>
2020-07-13 22:55:26 +0100

    [3668] add support for nested features

--- spec/services/feature_service_spec.rb
+++ spec/services/feature_service_spec.rb
@@ -1,18 +1,18 @@
 require "rails_helper"
 
 describe FeatureService do
-  let(:feature_enabled) { true }
+  let(:feature_value) { true }
 
   around(:each) do |example|
     old_value = Settings.features.rspec_testing
-    Settings.features.rspec_testing = feature_enabled
+    Settings.features.rspec_testing = feature_value
     example.run
     Settings.features.rspec_testing = old_value
   end
 
   describe ".require" do
     context "feature is enabled" do
-      let(:feature_enabled) { true }
+      let(:feature_value) { true }
 
       it "returns true" do
         response = FeatureService.require(:rspec_testing)
@@ -22,21 +22,37 @@ describe FeatureService do
     end
 
     context "feature is disabled" do
-      let(:feature_enabled) { false }
+      let(:feature_value) { false }
 
       it "raises an error" do
         expect { FeatureService.require(:rspec_testing) }
-          .to raise_error(
-            RuntimeError,
-            "Feature rspec_testing is disabled",
-          )
+          .to raise_error(RuntimeError, "Feature rspec_testing is disabled")
+      end
+    end
+
+    context "nested feature is enabled" do
+      let(:feature_value) { Config::Options.new nested: true }
+
+      it "returns true" do
+        response = FeatureService.require("rspec_testing.nested")
+
+        expect(response).to be_truthy
+      end
+    end
+
+    context "nested feature is disabled" do
+      let(:feature_value) { Config::Options.new nested: false }
+
+      it "raises an error" do
+        expect { FeatureService.require("rspec_testing.nested") }
+          .to raise_error(RuntimeError, "Feature rspec_testing.nested is disabled")
       end
     end
   end
 
   describe ".enabled?" do
     context "feature is enabled" do
-      let(:feature_enabled) { true }
+      let(:feature_value) { true }
 
       it "returns true" do
         response = FeatureService.enabled?(:rspec_testing)
@@ -46,10 +62,30 @@ describe FeatureService do
     end
 
     context "feature is disabled" do
-      let(:feature_enabled) { false }
+      let(:feature_value) { false }
 
       it "returns false" do
         response = FeatureService.enabled?(:rspec_testing)
+
+        expect(response).to be_falsey
+      end
+    end
+
+    context "nested feature is enabled" do
+      let(:feature_value) { Config::Options.new nested: true }
+
+      it "looks up the feature using dot-separated segments" do
+        response = FeatureService.enabled?("rspec_testing.nested")
+
+        expect(response).to be_truthy
+      end
+    end
+
+    context "nested feature is disabled" do
+      let(:feature_value) { Config::Options.new nested: false }
+
+      it "looks up the feature using dot-separated segments" do
+        response = FeatureService.enabled?("rspec_testing.nested")
 
         expect(response).to be_falsey
       end
