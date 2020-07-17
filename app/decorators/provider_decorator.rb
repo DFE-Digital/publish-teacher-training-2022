f3b6971a1bb4043cfd55bbd40ea1d020240014a5
Alistair Laing <aliuk2012@users.noreply.github.com>
2020-07-17 13:57:25 +0100

    [3686] Switch to using `fields`
    `fields` give us the benefit of not hard coding all the attribute names
    and values. As apart of this change I had to switch the `form_with` to
    use the decorated version instead of undecorated version `@provider`.
    
    As provider.accredited body is just a sorted hash, we needed to call
    `Provider.new` to convert it to a model which `fields` could use.
    
    Co-authored-by: De Fong <de_fong2k@yahoo.com>

--- app/decorators/provider_decorator.rb
+++ app/decorators/provider_decorator.rb
@@ -2,7 +2,9 @@ class ProviderDecorator < ApplicationDecorator
   delegate_all
 
   def accredited_bodies
-    object.accredited_bodies.sort_by { |provider| provider["provider_name"] }
+    object.accredited_bodies.sort_by { |provider| provider["provider_name"] }.map do |provider|
+      Provider.new(provider)
+    end
   end
 
   def website
