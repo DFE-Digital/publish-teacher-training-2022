e92dcd1d5d22a334c11fdb68e94fc8f667599e65
Phil Lee <asmega@gmail.com>
2020-08-03 15:36:25 +0100

    [3685] Add coverage for magic link signout path

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -69,5 +69,31 @@ describe SessionsController, type: :controller do
         expect(response).to redirect_to("/personas")
       end
     end
+
+    context "when using magic link" do
+      before do
+        allow(Settings.features).to receive(:signin_intercept).and_return(true)
+        allow(Settings.features).to receive(:signin_by_email).and_return(true)
+        allow(Settings.features).to receive(:dfe_signin).and_return(false)
+
+        session[:auth_user] = {
+          "uid" => "user@example.com",
+          "user_id" => "some-id",
+          "info" => {
+            "email" => "user@example.com",
+          },
+        }
+      end
+
+      it "resets the session" do
+        get :signout
+        expect(session[:auth_user]).to be_nil
+      end
+
+      it "redirects to /" do
+        get :signout
+        expect(response).to redirect_to("/")
+      end
+    end
   end
 end
