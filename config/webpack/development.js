a46cbc99b65a298778e9eb7a6afbc60d427b0a24
Alistair Laing <aliuk2012@users.noreply.github.com>
2020-08-17 11:25:59 +0100

    No need to make local hosted asset use https

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,12 +1,5 @@
-const fs = require('fs')
-
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
-
-environment.config.devServer.https = {
-  key: './config/localhost/https/localhost.key',
-  cert: './config/localhost/https/localhost.crt'
-}
 
 module.exports = environment.toWebpackConfig()
