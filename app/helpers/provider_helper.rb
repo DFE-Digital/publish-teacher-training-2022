4af98fa78562fc9914c6e82254d99aab33606cf4
David Kewal <davidrk@hey.com>
2020-09-10 14:51:42 +0100

    [34] Refactor google form url helper code to factor in next cycle

--- app/helpers/provider_helper.rb
+++ app/helpers/provider_helper.rb
@@ -1,6 +1,6 @@
 module ProviderHelper
-  def add_course_url(email, provider, is_current_cycle:)
-    cycle_key = is_current_cycle ? "current_cycle" : "next_cycle"
+  def add_course_url(email, provider)
+    cycle_key = is_current_cycle(provider.recruitment_cycle_year) ? "current_cycle" : "next_cycle"
 
     if provider.accredited_body?
       google_form_url_for(Settings.google_forms[cycle_key].new_course_for_accredited_bodies, email, provider)
@@ -14,5 +14,9 @@ private
   def google_form_url_for(settings, email, provider)
     settings.url + "&" +
       { settings.email_entry => email, settings.provider_code_entry => provider.provider_code }.to_query
+  end
+
+  def is_current_cycle(cycle_year)
+    Settings.current_cycle == cycle_year.to_i
   end
 end
