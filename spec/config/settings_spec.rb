57d9c972bd69ab4b946b20afc8e77f510c4b2ad5
De Fong <de_fong2k@yahoo.com>
2020-11-11 15:50:43 +0000

    [378] Added tests

--- spec/config/settings_spec.rb
+++ spec/config/settings_spec.rb
@@ -1,11 +1,24 @@
 require "rails_helper"
 
 describe Settings do
-  describe "authentication" do
+  settings = YAML.load_file(Rails.root.join("config/settings.yml")).with_indifferent_access
+
+  subject do
+    settings
+  end
+
+  describe "settings.teacher_training_api" do
     subject do
-      YAML.load_file(Rails.root.join("config/settings.yml"))
+      settings[:teacher_training_api]
     end
 
-    its(%w[current_cycle]) { should eq 2021 }
+    its(%w[algorithm]) { should_not be_blank }
+    its(%w[secret]) { should_not be_blank }
+    its(%w[base_url]) { should_not be_blank }
+    its(%w[issuer]) { should_not be_blank }
+    its(%w[audience]) { should_not be_blank }
+    its(%w[subject]) { should_not be_blank }
   end
+
+  its(%w[current_cycle]) { should eq 2021 }
 end
---
+++ spec/services/jwt/encode_service_spec.rb
@@ -0,0 +1,57 @@
+require "rails_helper"
+
+describe JWT::EncodeService do
+  let(:email) { "bat@localhost" }
+  let(:payload) { { "email" => email } }
+  let(:now) { 1_605_093_071 }
+
+  let(:expected_token_values) do
+    {
+      "data" => payload,
+      **claims,
+    }
+  end
+
+  let(:claims) do
+    {
+      "aud" => Settings.teacher_training_api.audience,
+      "exp" => (now + 5.minutes).to_i,
+      "iat" => now.to_i,
+      "iss" => Settings.teacher_training_api.issuer,
+      "sub" => Settings.teacher_training_api.subject,
+    }
+  end
+
+  let(:decoded_token) do
+    JWT.decode(
+      subject,
+      Settings.teacher_training_api.secret,
+      false, # NOTE: do not verify, it is the client responsibilities
+    ).first
+  end
+
+  let(:static_decoded_token) do
+    token = "eyJhbGciOiJIUzI1NiJ9.eyJkYXRhIjp7ImVtYWlsIjoiYmF0QGxvY2FsaG9zdCJ9LCJhdWQiOiJ0ZWFjaGVyLXRyYWluaW5nLWFwaSIsImV4cCI6MTYwNTA5MzM3MSwiaWF0IjoxNjA1MDkzMDcxLCJpc3MiOiJwdWJsaXNoLXRlYWNoZXItdHJhaW5pbmciLCJzdWIiOiJhY2Nlc3MifQ.njFc7S3opebXsOkkCM3UMCACfpxJmgsDIrvj2eJvBLY"
+    JWT.decode(
+      token,
+      Settings.teacher_training_api.secret,
+      false, # NOTE: do not verify, it is the client responsibilities
+    ).first
+  end
+
+  subject do
+    described_class.call(payload: payload)
+  end
+
+  describe "#call" do
+    before :each do
+      allow(Time).to receive(:zone)
+        .and_return(OpenStruct.new(now: now))
+    end
+
+    it "token values are equal" do
+      expect(decoded_token).to match(expected_token_values)
+      expect(static_decoded_token).to match(expected_token_values)
+    end
+  end
+end
