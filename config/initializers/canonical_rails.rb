1e83eb6daf2cf1dd49549746f7fefc0270c12246
Graham Pengelly <graham@three-tier.com>
2020-12-14 14:41:56 +0000

    Update canonical-rails initializer
    `whitelisted_parameters` -> `allowed_parameters`

--- config/initializers/canonical_rails.rb
+++ config/initializers/canonical_rails.rb
@@ -21,7 +21,7 @@ CanonicalRails.setup do |config|
   # Parameter spamming can cause index dilution by creating seemingly different URLs with identical or near-identical content.
   # Unless whitelisted, these parameters will be omitted
 
-  config.whitelisted_parameters # = []
+  config.allowed_parameters # = []
 
   # Output a matching OpenGraph URL meta tag (og:url) with the canonical URL, as recommended by Facebook et al
   config.opengraph_url = true
