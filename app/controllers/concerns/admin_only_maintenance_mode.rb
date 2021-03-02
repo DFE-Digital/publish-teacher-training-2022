3d4a3dd1260f99b3b34b71995ae12535417a0fd7
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2021-03-02 17:31:11 +0000

    [1148] maintenance on message added

--- app/controllers/concerns/admin_only_maintenance_mode.rb
+++ app/controllers/concerns/admin_only_maintenance_mode.rb
@@ -1,5 +1,6 @@
 module AdminOnlyMaintenanceMode
   extend ActiveSupport::Concern
+  include ActionView::Helpers::TextHelper
 
   included do
     before_action :redirect_non_admins_in_maintenance_mode
@@ -10,7 +11,7 @@ private
   def redirect_non_admins_in_maintenance_mode
     return unless Settings.features.maintenance_mode.enabled
 
-    flash[:notice] = Settings.features.maintenance_mode.message
+    flash[:notice] = simple_format(Settings.features.maintenance_mode.message)
 
     return if %w[sign_in sessions].include?(controller_name)
 
