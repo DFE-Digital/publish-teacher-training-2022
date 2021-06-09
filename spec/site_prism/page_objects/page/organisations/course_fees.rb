844c792d9bcea867f283f806afd9bab5fc9c33ca
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2021-06-09 14:37:29 +0100

    Use form builder for fee and salary forms

--- spec/site_prism/page_objects/page/organisations/course_fees.rb
+++ spec/site_prism/page_objects/page/organisations/course_fees.rb
@@ -5,14 +5,14 @@ module PageObjects
         set_url "/organisations/{provider_code}/{recruitment_cycle_year}/courses/{course_code}/fees"
 
         element :enrichment_form, '[data-qa="enrichment-form"]'
-        element :course_length_one_year, "#course_course_length_oneyear"
-        element :course_length_two_years, "#course_course_length_twoyears"
-        element :course_length_other, "#course_course_length_other"
-        element :course_length_other_length, "#course_course_length_other_length"
-        element :course_fees_uk_eu, "#course_fee_uk_eu"
-        element :course_fees_international, "#course_fee_international"
-        element :fee_details, "#course_fee_details"
-        element :financial_support, "#course_financial_support"
+        element :course_length_one_year, '[data-qa="course_course_length_oneyear"]'
+        element :course_length_two_years, '[data-qa="course_course_length_twoyears"]'
+        element :course_length_other, '[data-qa="course_course_length_other"]'
+        element :course_length_other_length, '[data-qa="course_course_length_other_length"]'
+        element :course_fees_uk_eu, '[data-qa="course_fee_uk_eu"]'
+        element :course_fees_international, '[data-qa="course_fee_international"]'
+        element :fee_details, '[data-qa="course_fee_details"]'
+        element :financial_support, '[data-qa="course_financial_support"]'
       end
     end
   end
