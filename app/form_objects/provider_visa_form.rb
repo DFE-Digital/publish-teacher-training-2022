788f4dcef2b318b3157e9e1a3fff6d6c0eb5a0ba
Steve Hook <steve.hook@gmail.com>
2021-06-16 23:49:30 +0100

    Update app/form_objects/provider_visa_form.rb
    Co-authored-by: Frankie Roberto <frankie@frankieroberto.com>

--- app/form_objects/provider_visa_form.rb
+++ app/form_objects/provider_visa_form.rb
@@ -3,8 +3,8 @@ class ProviderVisaForm
 
   attr_reader :can_sponsor_skilled_worker_visa, :can_sponsor_student_visa
 
-  validates :can_sponsor_skilled_worker_visa, inclusion: { in: [true, false], message: "Select whether your provider can sponsor skilled worker visas" }
-  validates :can_sponsor_student_visa, inclusion: { in: [true, false], message: "Select whether your provider can sponsor student visas" }
+  validates :can_sponsor_skilled_worker_visa, inclusion: { in: [true, false], message: "Select if you can sponsor Skilled Worker visas" }
+  validates :can_sponsor_student_visa, inclusion: { in: [true, false], message: "Select if you can sponsor Student visas" }
 
   def save(provider)
     if valid?
