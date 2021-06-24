a8c56a4ec9b8a4e1f0688588036fd3ca121d5689
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2021-06-24 22:57:37 +0100

    Use form builder for course level and SEND (WIP)

--- spec/site_prism/page_objects/page/organisations/courses/new_level_page.rb
+++ spec/site_prism/page_objects/page/organisations/courses/new_level_page.rb
@@ -6,9 +6,9 @@ module PageObjects
           set_url "/organisations/{provider_code}/{recruitment_cycle_year}/courses/level/new"
 
           section :level_fields, '[data-qa="course__level"]' do
-            element :primary,           "#course_level_primary"
-            element :secondary,         "#course_level_secondary"
-            element :further_education, "#course_level_further_education"
+            element :primary, '[data-qa="course__primary"]'
+            element :secondary, '[data-qa="course__secondary"]'
+            element :further_education, '[data-qa="course__further_education"]'
           end
         end
       end
