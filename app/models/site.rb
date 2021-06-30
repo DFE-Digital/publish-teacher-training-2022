5297d4afa089a51bc77d144b5559f7d103d6eefe
David <davidrk@hey.com>
2021-06-30 15:27:34 +0100

    [2122] Add URN column to locations table

--- app/models/site.rb
+++ app/models/site.rb
@@ -3,7 +3,7 @@ class Site < Base
   belongs_to :provider, param: :provider_code
   has_one :site_status
 
-  properties :code, :location_name, :address1, :address2, :address3
+  properties :code, :location_name, :address1, :address2, :address3, :urn
   properties :address4, :postcode, :latitude, :longitude, :travel_to_work_area, :london_borough
 
   REGIONS = [
