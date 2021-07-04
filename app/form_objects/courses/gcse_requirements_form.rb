c0297a33feb03ffd8c9f1020e25f972906d5b9b1
George Holborn <george.holborn@digital.education.gov.uk>
2021-07-04 23:37:35 +0100

    Add checkbox validations to GCSE form
    Inline with provided content we need to validate checkboxes if no
    details are given after user confirms yes to equivalencies. There is
    some subtlty here in that different validations are required for the
    differing primary and secondary form layouts.

--- app/form_objects/courses/gcse_requirements_form.rb
+++ app/form_objects/courses/gcse_requirements_form.rb
@@ -4,11 +4,12 @@ module Courses
     include ActiveModel::Validations::Callbacks
 
     attr_accessor :accept_pending_gcse, :accept_gcse_equivalency, :accept_english_gcse_equivalency,
-                  :accept_maths_gcse_equivalency, :accept_science_gcse_equivalency, :additional_gcse_equivalencies
+                  :accept_maths_gcse_equivalency, :accept_science_gcse_equivalency, :additional_gcse_equivalencies, :level
 
     validates :accept_pending_gcse, inclusion: { in: [true, false], message: "Select if you consider candidates with pending GCSEs" }
     validates :accept_gcse_equivalency, inclusion: { in: [true, false], message: "Select if you consider candidates with pending equivalency tests" }
-    validates :additional_gcse_equivalencies, presence: { message: "Enter details about equivalency tests" }, if: -> { equivalency_details_not_given }
+    validate :primary_or_secondary_equivalency_details_not_given, if: -> { equivalencies_not_selected? }
+    validates :additional_gcse_equivalencies, presence: { message: "Enter details about equivalency tests" }, if: -> { equivalencies_not_selected? }
 
     def save(course)
       return false unless valid?
@@ -33,14 +34,22 @@ module Courses
         accept_maths_gcse_equivalency: course.accept_maths_gcse_equivalency,
         accept_science_gcse_equivalency: course.accept_science_gcse_equivalency,
         additional_gcse_equivalencies: course.additional_gcse_equivalencies,
+        level: course.level,
       )
     end
 
   private
 
-    def equivalency_details_not_given
-      accept_gcse_equivalency.present? && accept_english_gcse_equivalency.blank? &&
-        accept_maths_gcse_equivalency.blank? && accept_science_gcse_equivalency.blank?
+    def primary_or_secondary_equivalency_details_not_given
+      if level == "primary"
+        errors.add(:equivalencies, "Select if you accept equivalency tests in English maths or science")
+      else
+        errors.add(:equivalencies, "Select if you accept equivalency tests in English or maths")
+      end
+    end
+
+    def equivalencies_not_selected?
+      accept_gcse_equivalency.present? && accept_english_gcse_equivalency.blank? && accept_maths_gcse_equivalency.blank? && accept_science_gcse_equivalency.blank?
     end
 
     def set_equivalency_values_to_false
