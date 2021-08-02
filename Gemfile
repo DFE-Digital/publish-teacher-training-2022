DFE-Digital/manage-courses-frontend

Update govuk-components and address breaking changes

Gemfile
@@ -22,7 +22,7 @@ gem "aasm"
 gem "google-cloud-bigquery"
 
 # Used to build our forms and style them using govuk-frontend class names
-gem "govuk-components"
+gem "govuk-components", "2.0.0b5"
 gem "govuk_design_system_formbuilder"
 
 # View components are used to encapsulate logic in views

DFE-Digital/publish-teacher-training-2022

Gemfile
@@ -22,7 +22,7 @@ gem "aasm"
 gem "google-cloud-bigquery"
 
 # Used to build our forms and style them using govuk-frontend class names
-gem "govuk-components"
+gem "govuk-components", "2.0.0b7"
 gem "govuk_design_system_formbuilder"
 
 # View components are used to encapsulate logic in views