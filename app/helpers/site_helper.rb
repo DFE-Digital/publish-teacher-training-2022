1e3cd293c4f2c18b97eb5f9ea4bbf62f5dd7c0e0
David Kewal <davidrk@hey.com>
2022-01-07 14:35:15 +0000

    515] Add helper to generate links to new publish

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -2,4 +2,8 @@ module SiteHelper
   def urn_required?(recruitment_cycle_year)
     recruitment_cycle_year >= Site::URN_2022_REQUIREMENTS_REQUIRED_FROM
   end
+
+  def new_publish_link_for(path)
+    "#{Settings.new_publish_url}/publish#{path}"
+  end
 end
